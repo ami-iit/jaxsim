5fcf409200db1992556b00bbf6fcc846c941e812
Diego Ferigo <diego.ferigo@iit.it>
2022-05-17 11:30:59 +0200

    Define default gravity

---
+++ src/jaxsim/physics/__init__.py
@@ -0,0 +1,13 @@
+import numpy.typing
+
+from . import algos, model
+
+
+def default_gravity() -> numpy.typing.NDArray:
+
+    import jax.numpy as jnp
+
+    return jnp.array([0, 0, -9.80])
+
+
+# from . import dyn, models, spatial, threed, utils

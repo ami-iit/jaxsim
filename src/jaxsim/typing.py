c6817beea52b2e7a080b6b69a0d3e468cd772098
Diego Ferigo <diego.ferigo@iit.it>
2022-05-17 11:28:55 +0200

    Add typing module

---
+++ src/jaxsim/typing.py
@@ -0,0 +1,28 @@
+from typing import Any, Dict, Hashable, List, NamedTuple, Tuple, Union
+
+import jax.numpy as jnp
+import numpy as np
+import numpy.typing as npt
+
+# JAX types
+FloatJax = Union[jnp.float16, jnp.float32, jnp.float64]
+ArrayJax = jnp.ndarray
+TensorJax = jnp.ndarray
+VectorJax = ArrayJax
+MatrixJax = ArrayJax
+PyTree = Union[
+    TensorJax,
+    Dict[Hashable, "PyTree"],
+    List["PyTree"],
+    NamedTuple,
+    Tuple["PyTree"],
+    None,
+    Any,
+]
+
+# Mixed JAX / NumPy types
+Array = Union[npt.NDArray, ArrayJax]
+Tensor = Union[npt.NDArray, ArrayJax]
+Vector = Array
+Matrix = Array
+Float = Union[float, np.float, FloatJax]

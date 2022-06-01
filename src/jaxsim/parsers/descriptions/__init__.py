9a065d24c0b3508aa328ee575d567ec9be144dcb
Diego Ferigo <diego.ferigo@iit.it>
2022-06-01 18:14:44 +0200

    Add support of sphere collision shape

--- src/jaxsim/parsers/descriptions/__init__.py
+++ src/jaxsim/parsers/descriptions/__init__.py
@@ -1,4 +1,4 @@
-from .collision import BoxCollision, CollidablePoint, CollisionShape
+from .collision import BoxCollision, CollidablePoint, CollisionShape, SphereCollision
 from .joint import JointDescription, JointDescriptor, JointGenericAxis, JointType
 from .link import LinkDescription
 from .model import ModelDescription

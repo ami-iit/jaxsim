cc6c68db16ddfc2d68f081240af3387f4112eaac
Diego Ferigo <diego.ferigo@iit.it>
2022-06-03 12:35:46 +0200

    Add physics model

---
+++ src/jaxsim/physics/model/physics_model.py
@@ -0,0 +1,265 @@
+import dataclasses
+from typing import Dict, List, Union
+
+import jax.numpy as jnp
+import jax_dataclasses
+import numpy as np
+from jax_dataclasses import pytree_dataclass, static_field
+
+import jaxsim.parsers
+import jaxsim.physics
+import jaxsim.typing as jtp
+from jaxsim.math.plucker import Plucker
+from jaxsim.parsers.descriptions import JointDescriptor, JointType
+from jaxsim.physics import default_gravity
+from jaxsim.utils import tracing
+
+from .ground_contact import GroundContact
+from .physics_model_state import PhysicsModelState
+
+
+@pytree_dataclass
+class PhysicsModel:
+
+    NB: int = static_field()
+    initial_state: PhysicsModelState = jax_dataclasses.field(default=None)
+    gravity: jtp.Vector = dataclasses.field(
+        default_factory=lambda: jnp.hstack(
+            [np.zeros(3), jaxsim.physics.default_gravity()]
+        )
+    )
+    is_floating_base: bool = static_field(default_factory=lambda: False)
+    gc: GroundContact = dataclasses.field(default_factory=lambda: GroundContact())
+    description: jaxsim.parsers.descriptions.model.ModelDescription = static_field(
+        default=None
+    )
+
+    _parent_array_dict: Dict[int, int] = static_field(default_factory=dict)
+    _jtype_dict: Dict[int, Union[JointType, JointDescriptor]] = static_field(
+        default_factory=dict
+    )
+    _tree_transforms_dict: Dict[int, jtp.Matrix] = dataclasses.field(
+        default_factory=dict
+    )
+    _link_inertias_dict: Dict[int, jtp.Matrix] = dataclasses.field(default_factory=dict)
+
+    def __post_init__(self):
+
+        if self.initial_state is None:
+            initial_state = PhysicsModelState.zero(physics_model=self)
+            object.__setattr__(self, "initial_state", initial_state)
+
+    @staticmethod
+    def build_from(
+        model_description: jaxsim.parsers.descriptions.model.ModelDescription,
+        gravity: jtp.Vector = default_gravity(),
+    ):
+
+        if gravity.size != 3:
+            raise ValueError(gravity.size)
+
+        # ===================================
+        # Initialize physics model parameters
+        # ===================================
+
+        # Get the number of bodies, including the base link
+        num_of_bodies = len(model_description)
+
+        # Build the parent array λ of the floating-base model.
+        # Note: the parent of the base link is not set since it's not defined.
+        parent_array_dict = {
+            link.index: link.parent.index
+            for link in model_description
+            if link.parent is not None
+        }
+
+        # Get the 6D inertias of all links
+        link_spatial_inertias_dict = {
+            link.index: link.inertia for link in iter(model_description)
+        }
+
+        # Dict from the joint index to its type.
+        # Note: the joint index is equal to its child link index.
+        joint_types_dict = {
+            joint.index: joint.jtype for joint in model_description.joints
+        }
+
+        # Transform between model's root and model's base link
+        # (this is just the pose of the base link in the SDF description)
+        base_link = model_description.links_dict[model_description.link_names()[0]]
+        R_H_B = model_description.transform(name=base_link.name)
+        B_H_R = np.linalg.inv(R_H_B)
+        tree_transform_0 = Plucker.from_transform(transform=B_H_R)
+
+        # Compute the tree transforms: pre(i)_X_λ(i).
+        # Given a joint 'i', it is the coordinate transform between its predecessor
+        # frame [pre(i)] and the frame of its parent link [λ(i)].
+        tree_transforms_dict = {
+            0: tree_transform_0,
+            **{
+                j.index: Plucker.from_transform(
+                    transform=model_description.relative_transform(
+                        relative_to=j.name, name=j.parent.name
+                    )
+                )
+                for j in model_description.joints
+            },
+        }
+
+        # =======================
+        # Build the initial state
+        # =======================
+
+        # Initial joint positions
+        q0 = jnp.array(
+            [
+                model_description.joints_dict[j.name].initial_position
+                for j in model_description.joints
+            ]
+        )
+
+        # Build the initial state
+        initial_state = PhysicsModelState(
+            joint_positions=q0,
+            joint_velocities=jnp.zeros_like(q0),
+            base_position=model_description.root_pose.root_position,
+            base_quaternion=model_description.root_pose.root_quaternion,
+        )
+
+        # =======================
+        # Build the physics model
+        # =======================
+
+        # Initialize the model
+        physics_model = PhysicsModel(
+            NB=num_of_bodies,
+            initial_state=initial_state,
+            _parent_array_dict=parent_array_dict,
+            _jtype_dict=joint_types_dict,
+            _tree_transforms_dict=tree_transforms_dict,
+            _link_inertias_dict=link_spatial_inertias_dict,
+            gravity=jnp.hstack([np.zeros(3), gravity.squeeze()]),
+            is_floating_base=True,
+            gc=GroundContact.build_from(model_description=model_description),
+            description=(model_description),
+        )
+
+        # Floating-base models
+        if not model_description.fixed_base:
+            return physics_model
+
+        # Fixed-base models
+        with jax_dataclasses.copy_and_mutate(physics_model) as physics_model_fixed:
+            physics_model_fixed.is_floating_base = False
+
+        return physics_model_fixed
+
+    def dofs(self) -> int:
+
+        return len(list(self._jtype_dict.keys()))
+
+    @property
+    def parent(self) -> jtp.Vector:
+        return self.parent_array()
+
+    def parent_array(self) -> jtp.Vector:
+        """Returns λ(i)"""
+
+        return jnp.array([-1] + list(self._parent_array_dict.values()))
+
+    def support_body_array(self, body_index: int) -> jtp.Vector:
+        """Returns κ(i)"""
+
+        kappa: List[int] = [body_index]
+
+        if body_index == 0:
+            return np.array(kappa)
+
+        while True:
+
+            i = self._parent_array_dict[kappa[-1]]
+
+            if i == 0:
+                break
+
+            kappa.append(i)
+
+        kappa.append(0)
+        return np.array(list(reversed(kappa)), dtype=int)
+
+    @property
+    def tree_transforms(self) -> jtp.Array:
+
+        X_tree = jnp.array(
+            [
+                self._tree_transforms_dict.get(idx, jnp.eye(6))
+                for idx in np.arange(start=0, stop=self.NB)
+            ]
+        )
+
+        return X_tree
+
+    @property
+    def spatial_inertias(self) -> jtp.Array:
+
+        M_links = jnp.array(
+            [
+                self._link_inertias_dict.get(idx, jnp.zeros(6))
+                for idx in np.arange(start=0, stop=self.NB)
+            ]
+        )
+
+        return M_links
+
+    def jtype(self, joint_index: int) -> JointType:
+
+        if joint_index == 0 or joint_index >= self.NB:
+            raise ValueError(joint_index)
+
+        return self._jtype_dict[joint_index]
+
+    def joint_transforms(self, q: jtp.Vector) -> jtp.Array:
+
+        from jaxsim.math.joint import jcalc
+
+        if not tracing(q) and q.shape[0] != self.dofs():
+            raise ValueError(q.shape)
+
+        Xj = jnp.stack(
+            [jnp.zeros(shape=(6, 6))]
+            + [
+                jcalc(jtyp=self.jtype(index + 1), q=joint_position)[0]
+                for index, joint_position in enumerate(q)
+            ]
+        )
+
+        return Xj
+
+    def motion_subspaces(self, q: jtp.Vector) -> jtp.Array:
+
+        from jaxsim.math.joint import jcalc
+
+        if not tracing(q) and q.shape[0] != self.dofs():
+            raise ValueError(q.shape)
+
+        SS = jnp.stack(
+            [jnp.vstack(jnp.zeros(6))]
+            + [
+                jcalc(jtyp=self.jtype(index + 1), q=joint_position)[1]
+                for index, joint_position in enumerate(q)
+            ]
+        )
+
+        return SS
+
+    def __eq__(self, other: "PhysicsModel") -> bool:
+
+        same = True
+        same = same and self.NB == other.NB
+        same = same and np.allclose(self.gravity, other.gravity)
+
+        return same
+
+    def __hash__(self):
+
+        return hash(self.__repr__())
---
+++ src/jaxsim/physics/model/physics_model_state.py
@@ -0,0 +1,160 @@
+import jax.numpy as jnp
+import jax_dataclasses
+
+import jaxsim.physics.model.physics_model
+import jaxsim.typing as jtp
+
+
+@jax_dataclasses.pytree_dataclass
+class PhysicsModelState:
+
+    # Joint state
+    joint_positions: jtp.Vector
+    joint_velocities: jtp.Vector
+
+    # Base state
+    base_position: jtp.Vector = jax_dataclasses.field(
+        default_factory=lambda: jnp.zeros(3)
+    )
+    base_quaternion: jtp.Vector = jax_dataclasses.field(
+        default_factory=lambda: jnp.array([1.0, 0, 0, 0])
+    )
+    base_linear_velocity: jtp.Vector = jax_dataclasses.field(
+        default_factory=lambda: jnp.zeros(3)
+    )
+    base_angular_velocity: jtp.Vector = jax_dataclasses.field(
+        default_factory=lambda: jnp.zeros(3)
+    )
+
+    @staticmethod
+    def zero(
+        physics_model: "jaxsim.physics.model.physics_model.PhysicsModel",
+    ) -> "PhysicsModelState":
+
+        return PhysicsModelState(
+            joint_positions=jnp.zeros(physics_model.dofs()),
+            joint_velocities=jnp.zeros(physics_model.dofs()),
+        )
+
+    def xfb(self) -> jtp.Array:
+
+        return jnp.hstack(
+            [
+                self.base_quaternion,
+                self.base_position,
+                self.base_angular_velocity,
+                self.base_linear_velocity,
+            ]
+        )
+
+    def replace(self, validate: bool = True, **kwargs) -> "PhysicsModelState":
+
+        with jax_dataclasses.copy_and_mutate(self, validate=validate) as updated_state:
+
+            _ = [updated_state.__setattr__(k, v) for k, v in kwargs.items()]
+
+        return updated_state
+
+    def valid(
+        self, physics_model: "jaxsim.physics.model.physics_model.PhysicsModel"
+    ) -> bool:
+
+        from jaxsim.simulation.utils import check_valid_shape
+
+        valid = True
+
+        valid = check_valid_shape(
+            what="joint_positions",
+            shape=self.joint_positions.shape,
+            expected_shape=(physics_model.dofs(),),
+            valid=valid,
+        )
+
+        valid = check_valid_shape(
+            what="joint_velocities",
+            shape=self.joint_velocities.shape,
+            expected_shape=(physics_model.dofs(),),
+            valid=valid,
+        )
+
+        valid = check_valid_shape(
+            what="base_position",
+            shape=self.base_position.shape,
+            expected_shape=(3,),
+            valid=valid,
+        )
+
+        valid = check_valid_shape(
+            what="base_quaternion",
+            shape=self.base_quaternion.shape,
+            expected_shape=(4,),
+            valid=valid,
+        )
+
+        valid = check_valid_shape(
+            what="base_linear_velocity",
+            shape=self.base_linear_velocity.shape,
+            expected_shape=(3,),
+            valid=valid,
+        )
+
+        valid = check_valid_shape(
+            what="base_angular_velocity",
+            shape=self.base_angular_velocity.shape,
+            expected_shape=(3,),
+            valid=valid,
+        )
+
+        return valid
+
+
+@jax_dataclasses.pytree_dataclass
+class PhysicsModelInput:
+
+    tau: jtp.VectorJax
+    f_ext: jtp.MatrixJax
+
+    @staticmethod
+    def zero(
+        physics_model: "jaxsim.physics.model.physics_model.PhysicsModel",
+    ) -> "PhysicsModelInput":
+
+        ode_input = PhysicsModelInput(
+            tau=jnp.zeros(physics_model.dofs()),
+            f_ext=jnp.zeros(shape=(physics_model.NB, 6)),
+        )
+
+        assert ode_input.valid(physics_model)
+        return ode_input
+
+    def replace(self, validate: bool = True, **kwargs) -> "PhysicsModelInput":
+
+        with jax_dataclasses.copy_and_mutate(self, validate=validate) as updated_input:
+
+            _ = [updated_input.__setattr__(k, v) for k, v in kwargs.items()]
+
+        return updated_input
+
+    def valid(
+        self, physics_model: "jaxsim.physics.model.physics_model.PhysicsModel"
+    ) -> bool:
+
+        from jaxsim.simulation.utils import check_valid_shape
+
+        valid = True
+
+        valid = check_valid_shape(
+            what="tau",
+            shape=self.tau.shape,
+            expected_shape=(physics_model.dofs(),),
+            valid=valid,
+        )
+
+        valid = check_valid_shape(
+            what="f_ext",
+            shape=self.f_ext.shape,
+            expected_shape=(physics_model.NB, 6),
+            valid=valid,
+        )
+
+        return valid

d770962825eb9b0ea59ebd509b059aabd2b519e3
Diego Ferigo <diego.ferigo@iit.it>
2022-06-03 12:34:04 +0200

    Replace scikit_bot dependency with python-sdformat

--- src/jaxsim/parsers/sdf/utils.py
+++ src/jaxsim/parsers/sdf/utils.py
@@ -3,34 +3,30 @@ from typing import Optional, Union
 import jax.numpy as jnp
 import numpy as np
 import numpy.typing as npt
+import pysdf
 from scipy.spatial.transform import Rotation as R
 
 from jaxsim.parsers import descriptions
 
 
-def from_sdf_string_list(string_list: str, epsilon: float = 1e-03) -> npt.NDArray:
+def from_sdf_string_list(string_list: str, epsilon: float = 1e-06) -> npt.NDArray:
 
     lst = np.array(string_list.split(" "), dtype=float)
     lst[np.abs(lst) < epsilon] = 0
     return lst
 
 
-def from_sdf_pose(pose: str) -> npt.NDArray:
-
-    # Parse the pose string
-    pose_array = from_sdf_string_list(pose)
-    assert pose_array.size == 6
-
-    # Extract position and Euler angles
-    position, rpy = pose_array[0:3], pose_array[3:6]
+def from_sdf_pose(pose: pysdf.Pose) -> npt.NDArray:
 
     # Transform euler to DCM matrix (sequence of extrinsic rotations, i.e. all angles
     # consider a fixed reference frame)
-    DCM = R.from_euler(seq="xyz", angles=rpy).as_matrix()
+    DCM = R.from_euler(
+        seq="xyz", angles=pose.orientation, degrees=pose.degrees
+    ).as_matrix()
 
     return np.vstack(
         [
-            np.hstack([DCM, np.vstack(position)]),
+            np.hstack([DCM, np.vstack(pose.position)]),
             np.array([0, 0, 0, 1]),
         ]
     )
@@ -67,23 +63,15 @@ def flip_velocity_serialization(array: npt.NDArray) -> npt.NDArray:
         return _np.block([[D, C], [B, A]])
 
 
-def from_sdf_inertial(inertial_sdf_element) -> npt.NDArray:
+def from_sdf_inertial(inertial: pysdf.Link.Inertial) -> npt.NDArray:
 
     from jaxsim.sixd import se3, so3
 
-    # Parse the inertial pose
-    inertial_pose = from_sdf_string_list(
-        string_list=inertial_sdf_element.pose, epsilon=1e-9
-    )
-
-    # Extract position and rpy
-    inertial_position, inertial_rpy = np.split(inertial_pose, 2)
-
     # Extract the "mass" element
-    m = inertial_sdf_element.mass
+    m = inertial.mass
 
     # Extract the "inertia" element
-    inertia_element = inertial_sdf_element.inertia
+    inertia_element = inertial.inertia
 
     # Build the 3x3 inertia matrix expressed in the CoM
     I_com = np.array(
@@ -105,12 +93,14 @@ def from_sdf_inertial(inertial_sdf_element) -> npt.NDArray:
     # Transform euler to DCM matrix (sequence of extrinsic rotations, i.e. all angles
     # consider a fixed reference frame)
     l_R_com = so3.SO3.from_matrix(
-        R.from_euler(seq="xyz", angles=inertial_rpy).as_matrix()
+        R.from_euler(
+            seq="xyz", angles=inertial.pose.orientation, degrees=inertial.pose.degrees
+        ).as_matrix()
     )
 
     # Compute the transform from the inertial frame (CoM) to the link frame
     l_H_com = se3.SE3.from_rotation_and_translation(
-        rotation=l_R_com, translation=inertial_position
+        rotation=l_R_com, translation=np.array(inertial.pose.position)
     )
 
     # We need its inverse
@@ -119,55 +109,60 @@ def from_sdf_inertial(inertial_sdf_element) -> npt.NDArray:
     # Express the CoM inertia matrix in the link frame
     I_expressed_in_link_frame = com_H_l.adjoint().T @ I_generalized @ com_H_l.adjoint()
 
-    # Convert lin-ang serialization to ang-lin used by featherstone
+    # Convert lin-ang serialization to ang-lin used by Featherstone
     I_link = flip_velocity_serialization(I_expressed_in_link_frame)
 
     return jnp.array(I_link)
 
 
 def axis_to_jtype(
-    axis: Optional[npt.NDArray], type: str
+    axis: pysdf.Joint.Axis, type: str
 ) -> Union[descriptions.JointType, descriptions.JointDescriptor]:
 
     if type == "fixed":
         return descriptions.JointType.F
 
-    if np.allclose(axis, [1, 0, 0]) and type in {"revolute", "continuous"}:
+    if not (axis.xyz is not None and axis.xyz.text is not None):
+        raise ValueError("Failed to read axis xyz data")
+
+    axis_xyz = from_sdf_string_list(axis.xyz.text)
+
+    if np.allclose(axis_xyz, [1, 0, 0]) and type in {"revolute", "continuous"}:
         return descriptions.JointType.Rx
 
-    if np.allclose(axis, [0, 1, 0]) and type in {"revolute", "continuous"}:
+    if np.allclose(axis_xyz, [0, 1, 0]) and type in {"revolute", "continuous"}:
         return descriptions.JointType.Ry
 
-    if np.allclose(axis, [0, 0, 1]) and type in {"revolute", "continuous"}:
+    if np.allclose(axis_xyz, [0, 0, 1]) and type in {"revolute", "continuous"}:
         return descriptions.JointType.Rz
 
-    if np.allclose(axis, [1, 0, 0]) and type == "prismatic":
+    if np.allclose(axis_xyz, [1, 0, 0]) and type == "prismatic":
         return descriptions.JointType.Px
 
-    if np.allclose(axis, [0, 1, 0]) and type == "prismatic":
+    if np.allclose(axis_xyz, [0, 1, 0]) and type == "prismatic":
         return descriptions.JointType.Py
 
-    if np.allclose(axis, [0, 0, 1]) and type == "prismatic":
+    if np.allclose(axis_xyz, [0, 0, 1]) and type == "prismatic":
         return descriptions.JointType.Pz
 
     if type == "revolute":
         return descriptions.JointGenericAxis(
-            code=descriptions.JointType.R, axis=np.array(axis, dtype=float)
+            code=descriptions.JointType.R, axis=np.array(axis_xyz, dtype=float)
         )
 
     if type == "prismatic":
         return descriptions.JointGenericAxis(
-            code=descriptions.JointType.P, axis=np.array(axis, dtype=float)
+            code=descriptions.JointType.P, axis=np.array(axis_xyz, dtype=float)
         )
 
-    raise ValueError("Joint not supported", axis, type)
+    raise ValueError("Joint not supported", axis_xyz, type)
 
 
 def create_box_collision(
-    collision_sdf_element, link_description: descriptions.LinkDescription
+    collision: pysdf.Collision, link_description: descriptions.LinkDescription
 ) -> descriptions.BoxCollision:
 
-    x, y, z = from_sdf_string_list(collision_sdf_element.geometry.box.size)
+    x, y, z = collision.geometry.box.size
 
     center = np.array([x / 2, y / 2, z / 2])
 
@@ -187,7 +182,7 @@ def create_box_collision(
         - center
     )
 
-    H = from_sdf_pose(pose=collision_sdf_element.pose.value)
+    H = from_sdf_pose(pose=collision.pose)
 
     center_wrt_link = (H @ np.hstack([center, 1.0]))[0:-1]
     box_corners_wrt_link = (
@@ -209,8 +204,8 @@ def create_box_collision(
 
 
 def create_sphere_collision(
-    collision_sdf_element, link_description: descriptions.LinkDescription
-) -> descriptions.BoxCollision:
+    collision: pysdf.Collision, link_description: descriptions.LinkDescription
+) -> descriptions.SphereCollision:
 
     # From https://stackoverflow.com/a/26127012
     def fibonacci_sphere(samples: int) -> npt.NDArray:
@@ -232,10 +227,10 @@ def create_sphere_collision(
 
         return np.vstack(points)
 
-    r = collision_sdf_element.geometry.sphere.radius
+    r = collision.geometry.sphere.radius
     sphere_points = r * fibonacci_sphere(samples=250)
 
-    H = from_sdf_pose(pose=collision_sdf_element.pose.value)
+    H = from_sdf_pose(pose=collision.pose)
 
     center_wrt_link = (H @ np.hstack([0, 0, 0, 1.0]))[0:-1]
 

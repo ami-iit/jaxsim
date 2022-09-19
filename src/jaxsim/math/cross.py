f1c60fa067cf21c1452cac5bcbf40e66265f5bc4
Diego Ferigo <diego.ferigo@iit.it>
2022-09-19 17:11:30 +0200

    Update computation of cross product on R^6

--- src/jaxsim/math/cross.py
+++ src/jaxsim/math/cross.py
@@ -2,21 +2,19 @@ import jax.numpy as jnp
 
 import jaxsim.typing as jtp
 
+from .skew import Skew
+
 
 class Cross:
     @staticmethod
     def vx(velocity_sixd: jtp.Vector) -> jtp.Matrix:
 
-        v = velocity_sixd.squeeze()
+        ω, v = jnp.split(velocity_sixd.squeeze(), 2)
 
-        v_cross = jnp.array(
+        v_cross = jnp.block(
             [
-                [0, -v[2], v[1], 0, 0, 0],
-                [v[2], 0, -v[0], 0, 0, 0],
-                [-v[1], v[0], 0, 0, 0, 0],
-                [0, -v[5], v[4], 0, -v[2], v[1]],
-                [v[5], 0, -v[3], v[2], 0, -v[0]],
-                [-v[4], v[3], 0, -v[1], v[0], 0],
+                [Skew.wedge(vector=ω), jnp.zeros(shape=(3, 3))],
+                [Skew.wedge(vector=v), Skew.wedge(vector=ω)],
             ]
         )
 

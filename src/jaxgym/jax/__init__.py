c37c126cc7926383ca2988ac4afb6e85b6b47e22
diegoferigo <diego.ferigo@iit.it>
2023-05-22 11:47:25 +0200

    [wip]

--- src/jaxgym/jax/__init__.py
+++ src/jaxgym/jax/__init__.py
@@ -1,5 +1,6 @@
 from .dataclass_func_env import JaxDataclassEnv
 from .dataclass_func_env_wrapper import JaxDataclassActionWrapper, JaxDataclassWrapper
 from .env import JaxEnv
-from .jaxsim_func_env import JaxSimFuncEnv
 from .pytree_space import PyTree
+
+# from .jaxsim_func_env import JaxSimFuncEnv

8922c379fb97590ce633b1daa96acd48105eadb5
diegoferigo <diego.ferigo@iit.it>
2023-06-09 11:28:16 +0200

    Update method to adjust algos inputs

--- src/jaxsim/physics/algos/utils.py
+++ src/jaxsim/physics/algos/utils.py
@@ -15,31 +15,42 @@ def process_inputs(
     tau: jtp.Vector = None,
     f_ext: jtp.Matrix = None,
 ) -> Tuple[jtp.Vector, jtp.Vector, jtp.Vector, jtp.Vector, jtp.Vector, jtp.Matrix]:
+    """
+    Adjust the inputs to the physics model.
+
+    Args:
+        physics_model: The physics model.
+        xfb: The variables of the base link.
+        q: The generalized coordinates.
+        qd: The generalized velocities.
+        qdd: The generalized accelerations.
+        tau: The generalized forces.
+        f_ext: The link external forces.
+
+    Returns:
+        The adjusted inputs.
+    """
+
     # Remove extra dimensions
-    q = q.squeeze() if q is not None else None
-    qd = qd.squeeze() if qd is not None else None
-    qdd = qdd.squeeze() if qdd is not None else None
-    tau = tau.squeeze() if tau is not None else None
-    xfb = xfb.squeeze() if xfb is not None else None
-    f_ext = f_ext.squeeze() if f_ext is not None else None
-
-    def fix_one_dof(vector: jtp.Vector) -> Optional[jtp.Vector]:
-        if vector is None:
-            return None
-
-        return jnp.array([vector]) if vector.shape == () else vector
+    q = q.squeeze() if q is not None else jnp.zeros(physics_model.dofs())
+    qd = qd.squeeze() if qd is not None else jnp.zeros(physics_model.dofs())
+    qdd = qdd.squeeze() if qdd is not None else jnp.zeros(physics_model.dofs())
+    tau = tau.squeeze() if tau is not None else jnp.zeros(physics_model.dofs())
+    xfb = xfb.squeeze() if xfb is not None else jnp.zeros(13).at[0].set(1)
+    f_ext = (
+        f_ext.squeeze()
+        if f_ext is not None
+        else jnp.zeros(shape=(physics_model.NB, 6)).squeeze()
+    )
 
     # Fix case with just 1 DoF
-    q = fix_one_dof(q)
-    qd = fix_one_dof(qd)
-    qdd = fix_one_dof(qdd)
-    tau = fix_one_dof(tau)
-
-    # Build matrix of external forces if not given
-    f_ext = f_ext if f_ext is not None else jnp.zeros(shape=(physics_model.NB, 6))
+    q = jnp.atleast_1d(q)
+    qd = jnp.atleast_1d(qd)
+    qdd = jnp.atleast_1d(qdd)
+    tau = jnp.atleast_1d(tau)
 
     # Fix case with just 1 body
-    f_ext = f_ext if physics_model.NB != 1 else f_ext.squeeze()[jnp.newaxis, ...]
+    f_ext = jnp.atleast_2d(f_ext)
 
     # Validate dimensions
     dofs = physics_model.dofs()

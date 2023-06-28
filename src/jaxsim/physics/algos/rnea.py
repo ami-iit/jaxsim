1d8df0e74ecab1615df0e4965ff818cdab35f1a4
diegoferigo <diego.ferigo@iit.it>
2023-06-28 14:19:42 +0200

    New RNEA implementation with jax.lax.scan

--- src/jaxsim/physics/algos/rnea.py
+++ src/jaxsim/physics/algos/rnea.py
@@ -1,5 +1,6 @@
-from typing import Dict, Tuple
+from typing import Tuple
 
+import jax
 import jax.numpy as jnp
 import numpy as np
 
@@ -20,6 +21,10 @@ def rnea(
     a0fb: jtp.Vector = jnp.zeros(6),
     f_ext: jtp.Matrix = None,
 ) -> Tuple[jtp.Vector, jtp.Vector]:
+    """
+    Recursive Newton-Euler Algorithm (RNEA) algorithm for inverse dynamics.
+    """
+
     xfb, q, qd, qdd, _, f_ext = utils.process_inputs(
         physics_model=model, xfb=xfb, q=q, qd=qd, qdd=qdd, f_ext=f_ext
     )
@@ -30,81 +35,130 @@ def rnea(
     if a0fb.shape[0] != 6:
         raise ValueError(a0fb.shape)
 
-    I = model.spatial_inertias
-    Xtree = model.tree_transforms
-    Xj = model.joint_transforms(q=q)
+    M = model.spatial_inertias
+    pre_X_λi = model.tree_transforms
+    i_X_pre = model.joint_transforms(q=q)
     S = model.motion_subspaces(q=q)
-    Xup = jnp.zeros_like(Xtree)
+    i_X_λi = jnp.zeros_like(pre_X_λi)
 
-    i_X_0 = jnp.zeros_like(Xtree)
+    i_X_0 = jnp.zeros_like(pre_X_λi)
     i_X_0 = i_X_0.at[0].set(jnp.eye(6))
 
-    v: Dict[int, jtp.VectorJax] = dict()
-    a: Dict[int, jtp.VectorJax] = dict()
-    f: Dict[int, jtp.VectorJax] = dict()
+    # Parent array mapping: i -> λ(i).
+    # Exception: λ(0) must not be used, it's initialized to -1.
+    λ = model.parent_array()
+
+    v = jnp.array([jnp.zeros([6, 1])] * model.NB)
+    a = jnp.array([jnp.zeros([6, 1])] * model.NB)
+    f = jnp.array([jnp.zeros([6, 1])] * model.NB)
 
     # 6D transform of base velocity
-    Xup_0 = B_X_W = Adjoint.from_quaternion_and_translation(
+    B_X_W = Adjoint.from_quaternion_and_translation(
         quaternion=xfb[0:4],
         translation=xfb[4:7],
         inverse=True,
         normalize_quaternion=True,
     )
-    Xup = Xup.at[0].set(Xup_0)
+    i_X_λi = i_X_λi.at[0].set(B_X_W)
 
-    v[0] = jnp.zeros(shape=(6, 1))
-    f[0] = jnp.zeros(shape=(6, 1))
-    a[0] = -B_X_W @ jnp.vstack(gravity)
+    a_0 = -B_X_W @ jnp.vstack(gravity)
+    a = a.at[0].set(a_0)
 
     if model.is_floating_base:
         W_v_WB = jnp.vstack(jnp.hstack([xfb[10:13], xfb[7:10]]))
-        v[0] = B_X_W @ W_v_WB
 
-        a[0] = B_X_W @ (jnp.vstack(a0fb) - jnp.vstack(gravity))
-        f[0] = (
-            I[0] @ a[0]
-            + Cross.vx_star(v[0]) @ I[0] @ v[0]
-            - jnp.linalg.inv(B_X_W).T @ jnp.vstack(f_ext[0])
+        v_0 = B_X_W @ W_v_WB
+        v = v.at[0].set(v_0)
+
+        a_0 = B_X_W @ (jnp.vstack(a0fb) - jnp.vstack(gravity))
+        a = a.at[0].set(a_0)
+
+        f_0 = (
+            M[0] @ a[0]
+            + Cross.vx_star(v[0]) @ M[0] @ v[0]
+            - Adjoint.inverse(B_X_W).T @ jnp.vstack(f_ext[0])
         )
+        f = f.at[0].set(f_0)
 
-    for i in np.arange(start=1, stop=model.NB):
+    ForwardPassCarry = Tuple[
+        jtp.MatrixJax, jtp.MatrixJax, jtp.MatrixJax, jtp.MatrixJax, jtp.MatrixJax
+    ]
+    forward_pass_carry = (i_X_λi, v, a, i_X_0, f)
+
+    def forward_pass(
+        carry: ForwardPassCarry, i: jtp.Int
+    ) -> Tuple[ForwardPassCarry, None]:
         ii = i - 1
+        i_X_λi, v, a, i_X_0, f = carry
 
         vJ = S[i] * qd[ii]
-        Xup_i = Xj[i] @ Xtree[i]
-        Xup = Xup.at[i].set(Xup_i)
+        i_X_λi_i = i_X_pre[i] @ pre_X_λi[i]
+        i_X_λi = i_X_λi.at[i].set(i_X_λi_i)
 
-        λi = model._parent_array_dict[i]
-        v[i] = Xup[i] @ v[λi] + vJ
-        a[i] = Xup[i] @ a[λi] + S[i] * qdd[ii] + Cross.vx(v[i]) @ vJ
+        v_i = i_X_λi[i] @ v[λ[i]] + vJ
+        v = v.at[i].set(v_i)
 
-        i_X_0_i = Xup[i] @ i_X_0[λi]
+        a_i = i_X_λi[i] @ a[λ[i]] + S[i] * qdd[ii] + Cross.vx(v[i]) @ vJ
+        a = a.at[i].set(a_i)
+
+        i_X_0_i = i_X_λi[i] @ i_X_0[λ[i]]
         i_X_0 = i_X_0.at[i].set(i_X_0_i)
-        i_X_W = jnp.linalg.inv(i_X_0[i] @ B_X_W).T
+        i_Xf_W = Adjoint.inverse(i_X_0[i] @ B_X_W).T
 
-        f[i] = (
-            I[i] @ a[i]
-            + Cross.vx_star(v[i]) @ I[i] @ v[i]
-            - i_X_W @ jnp.vstack(f_ext[i])
+        f_i = (
+            M[i] @ a[i]
+            + Cross.vx_star(v[i]) @ M[i] @ v[i]
+            - i_Xf_W @ jnp.vstack(f_ext[i])
         )
+        f = f.at[i].set(f_i)
+
+        return (i_X_λi, v, a, i_X_0, f), None
+
+    (i_X_λi, v, a, i_X_0, f), _ = jax.lax.scan(
+        f=forward_pass,
+        init=forward_pass_carry,
+        xs=np.arange(start=1, stop=model.NB),
+    )
 
     tau = jnp.zeros_like(q)
 
-    for i in reversed(np.arange(start=1, stop=model.NB)):
+    BackwardPassCarry = Tuple[jtp.MatrixJax, jtp.MatrixJax]
+    backward_pass_carry = (tau, f)
+
+    def backward_pass(
+        carry: BackwardPassCarry, i: jtp.Int
+    ) -> Tuple[BackwardPassCarry, None]:
         ii = i - 1
+        tau, f = carry
 
         value = S[i].T @ f[i]
         tau = tau.at[ii].set(value.squeeze())
 
-        λi = model._parent_array_dict[i]
+        def update_f(f: jtp.MatrixJax) -> jtp.MatrixJax:
+            f_λi = f[λ[i]] + i_X_λi[i].T @ f[i]
+            f = f.at[λ[i]].set(f_λi)
+            return f
 
-        if λi != 0 or model.is_floating_base:
-            f[λi] = f[λi] + Xup[i].T @ f[i]
+        f = jax.lax.cond(
+            pred=jnp.array([λ[i] != 0, model.is_floating_base]).any(),
+            true_fun=update_f,
+            false_fun=lambda f: f,
+            operand=f,
+        )
+
+        return (tau, f), None
+
+    (tau, f), _ = jax.lax.scan(
+        f=backward_pass,
+        init=backward_pass_carry,
+        xs=np.flip(np.arange(start=1, stop=model.NB)),
+    )
 
     # Handle 1 DoF models
     tau = jnp.atleast_1d(tau.squeeze())
     tau = jnp.vstack(tau) if tau.size > 0 else jnp.empty(shape=(0, 1))
 
+    # Express the base 6D force in the world frame
     W_f0 = B_X_W.T @ jnp.vstack(f[0])
 
     return W_f0, tau

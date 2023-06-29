Fix computation of the generalized free-floating jacobian

src/jaxsim/high_level/model.py
@@ -576,15 +576,53 @@ class Model(JaxsimDataclass):
     def generalized_velocity(self) -> jtp.Vector:
         return jnp.hstack([self.base_velocity(), self.joint_velocities()])
 
-    def generalized_jacobian(self, output_vel_repr: VelRepr = None) -> jtp.Matrix:
-        return jnp.vstack(
+    def generalized_free_floating_jacobian(
+        self, output_vel_repr: VelRepr = None
+    ) -> jtp.Matrix:
+        """"""
+
+        if output_vel_repr is None:
+            output_vel_repr = self.velocity_representation
+
+        # The body frame of the Link.jacobian method is the link frame L.
+        # In this method, we want instead to use the base link B as body frame.
+        # Therefore, we always get the link jacobian having Inertial as output
+        # representation, and then we convert it to the desired output representation.
+        if output_vel_repr is VelRepr.Inertial:
+            to_output = lambda J: J
+
+        elif output_vel_repr is VelRepr.Body:
+
+            def to_output(W_J_Wi):
+                W_H_B = self.base_transform()
+                B_X_W = sixd.se3.SE3.from_matrix(W_H_B).inverse().adjoint()
+                return B_X_W @ W_J_Wi
+
+        elif output_vel_repr is VelRepr.Mixed:
+
+            def to_output(W_J_Wi):
+                W_H_B = self.base_transform()
+                W_H_BW = jnp.array(W_H_B).at[0:3, 0:3].set(jnp.eye(3))
+                BW_X_W = sixd.se3.SE3.from_matrix(W_H_BW).inverse().adjoint()
+                return BW_X_W @ W_J_Wi
+
+        else:
+            raise ValueError(output_vel_repr)
+
+        # Get the link jacobians in Inertial representation and convert them to the
+        # target output representation in which the body frame is the base link B
+        J_free_floating = jnp.vstack(
             [
-                self.get_link(link_name=link_name).jacobian(
-                    output_vel_repr=output_vel_repr
+                to_output(
+                    self.get_link(link_name=link_name).jacobian(
+                        output_vel_repr=VelRepr.Inertial
+                    )
                 )
                 for link_name in self.link_names()
             ]
         )
+
+        return J_free_floating
 
     def free_floating_mass_matrix(self) -> jtp.Matrix:
         M_body = jaxsim.physics.algos.crba.crba(
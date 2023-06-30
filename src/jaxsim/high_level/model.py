b399c6a7eca3e6a91dd3dd03357eee5362200506
diegoferigo <diego.ferigo@iit.it>
2023-06-30 17:03:31 +0200

    Allow keeping the base pose when a model is reduced

--- src/jaxsim/high_level/model.py
+++ src/jaxsim/high_level/model.py
@@ -285,15 +285,20 @@ class Model(JaxsimDataclass):
 
         self._set_mutability(original_mutability)
 
-    def reduce(self, considered_joints: List[str]) -> None:
+    def reduce(
+        self, considered_joints: List[str], keep_base_pose: bool = False
+    ) -> None:
         """
         Reduce the model by lumping together the links connected by removed joints.
 
         Args:
             considered_joints: The list of joints to consider.
+            keep_base_pose: A flag indicating whether to keep the base pose or not.
         """
 
-        # Reduce the model description
+        # Reduce the model description.
+        # If considered_joints contains joints not existing in the model, the method
+        # will raise an exception.
         reduced_model_description = self.physics_model.description.reduce(
             considered_joints=considered_joints
         )
@@ -311,14 +316,22 @@ class Model(JaxsimDataclass):
             vel_repr=self.velocity_representation,
         )
 
-        # Replace the current model with the reduced one
-        original_mutability = self._mutability()
-        self._set_mutability(mutability=self._mutability().MUTABLE_NO_VALIDATION)
-        self.physics_model = reduced_model.physics_model
-        self.data = reduced_model.data
-        self._links = reduced_model._links
-        self._joints = reduced_model._joints
-        self._set_mutability(original_mutability)
+        # Extract the base pose
+        W_p_B = self.base_position()
+        W_Q_B = self.base_orientation(dcm=False)
+
+        # Replace the current model with the reduced model.
+        # Since the structure of the PyTree changes, we disable validation.
+        with self.mutable_context(mutability=Mutability.MUTABLE_NO_VALIDATION):
+            self.physics_model = reduced_model.physics_model
+            self.data = reduced_model.data
+            self._links = reduced_model._links
+            self._joints = reduced_model._joints
+
+        if keep_base_pose:
+            with self.mutable_context(mutability=Mutability.MUTABLE):
+                self.reset_base_position(position=W_p_B)
+                self.reset_base_orientation(orientation=W_Q_B, dcm=False)
 
     def zero(self) -> None:
         self.data = ModelData.zero(physics_model=self.physics_model)

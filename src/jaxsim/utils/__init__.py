68ac4b53b2c8fa06b088c17d89555aa00bc84318
diegoferigo <diego.ferigo@iit.it>
2023-08-04 00:16:02 +0200

    Create abstract class that can be automatically vectorized with vmap

--- src/jaxsim/utils/__init__.py
+++ src/jaxsim/utils/__init__.py
@@ -2,3 +2,4 @@ from jax_dataclasses._copy_and_mutate import _Mutability as Mutability
 
 from .jaxsim_dataclass import JaxsimDataclass
 from .tracing import not_tracing, tracing
+from .vmappable import Vmappable

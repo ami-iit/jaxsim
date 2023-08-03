6b424bf0ab7b0cef3ba80ddce189c292dff9df87
diegoferigo <diego.ferigo@iit.it>
2023-08-03 23:40:07 +0200

    Extend the tracing detection checking also the Tracer type

--- src/jaxsim/utils/tracing.py
+++ src/jaxsim/utils/tracing.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+import jax._src.core
 import jax.abstract_arrays
 import jax.flatten_util
 import jax.interpreters.partial_eval
@@ -12,6 +13,7 @@ def tracing(var: Any) -> bool | jax.Array:
         [
             isinstance(var, t)
             for t in (
+                jax._src.core.Tracer,
                 jax.abstract_arrays.ShapedArray,
                 jax.interpreters.partial_eval.DynamicJaxprTracer,
             )

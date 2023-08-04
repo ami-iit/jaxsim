565509ac36f7bc389406f4367a28c0d23a90bfd0
diegoferigo <diego.ferigo@iit.it>
2023-08-04 12:11:48 +0200

    Make high-level Link inherit from Vmappable with decorated methods

--- src/jaxsim/high_level/link.py
+++ src/jaxsim/high_level/link.py
@@ -1,6 +1,8 @@
 import dataclasses
+import functools
 from typing import Any
 
+import jax.lax
 import jax.numpy as jnp
 import jax_dataclasses
 import numpy as np
@@ -10,13 +12,13 @@ import jaxsim.parsers
 import jaxsim.sixd as sixd
 import jaxsim.typing as jtp
 from jaxsim.physics.algos.jacobian import jacobian
-from jaxsim.utils import JaxsimDataclass
+from jaxsim.utils import Vmappable, oop
 
 from .common import VelRepr
 
 
 @jax_dataclasses.pytree_dataclass
-class Link(JaxsimDataclass):
+class Link(Vmappable):
     """
     High-level class to operate on a single link of a simulated model.
     """
@@ -27,78 +29,127 @@ class Link(JaxsimDataclass):
 
     @property
     def parent_model(self) -> "jaxsim.high_level.model.Model":
+        """"""
+
         return self._parent_model
 
-    def valid(self) -> bool:
-        return self.parent_model is not None
+    @functools.partial(oop.jax_tf.method_ro, jit=False)
+    def valid(self) -> jtp.Bool:
+        """"""
+
+        return jnp.array(self.parent_model is not None, dtype=bool)
 
     # ==========
     # Properties
     # ==========
 
+    @functools.partial(oop.jax_tf.method_ro, jit=False, vmap=False)
     def name(self) -> str:
+        """"""
+
         return self.link_description.name
 
-    def index(self) -> int:
-        return self.link_description.index
+    @functools.partial(oop.jax_tf.method_ro, jit=False, vmap=False)
+    def index(self) -> jtp.Int:
+        """"""
+
+        return jnp.array(self.link_description.index, dtype=int)
 
     # ========
     # Dynamics
     # ========
 
+    @functools.partial(oop.jax_tf.method_ro, jit=False)
     def mass(self) -> jtp.Float:
-        return self.link_description.mass
+        """"""
 
+        return jnp.array(self.link_description.mass, dtype=float)
+
+    @functools.partial(oop.jax_tf.method_ro, jit=False)
     def spatial_inertia(self) -> jtp.Matrix:
-        return self.link_description.inertia
+        """"""
 
-    def com_position(self, in_link_frame: bool = True) -> jtp.VectorJax:
+        return jnp.array(self.link_description.inertia, dtype=float)
+
+    @functools.partial(oop.jax_tf.method_ro, vmap_in_axes=(0, None))
+    def com_position(self, in_link_frame: bool = True) -> jtp.Vector:
+        """"""
+
         from jaxsim.math.inertia import Inertia
 
         _, L_p_CoM, _ = Inertia.to_params(M=self.spatial_inertia())
 
-        if in_link_frame:
+        def com_in_link_frame():
             return L_p_CoM.squeeze()
 
-        W_H_L = self.transform()
-        W_ph_CoM = W_H_L @ jnp.hstack([L_p_CoM.squeeze(), 1])
+        def com_in_inertial_frame():
+            W_H_L = self.transform()
+            W_p̃_CoM = W_H_L @ jnp.hstack([L_p_CoM.squeeze(), 1])
 
-        return W_ph_CoM[0:3].squeeze()
+            return W_p̃_CoM[0:3].squeeze()
+
+        return jax.lax.select(
+            pred=in_link_frame,
+            on_true=com_in_link_frame(),
+            on_false=com_in_inertial_frame(),
+        )
 
     # ==========
     # Kinematics
     # ==========
 
+    @functools.partial(oop.jax_tf.method_ro)
     def position(self) -> jtp.Vector:
+        """"""
+
         return self.transform()[0:3, 3]
 
+    @functools.partial(oop.jax_tf.method_ro, static_argnames=["dcm"])
     def orientation(self, dcm: bool = False) -> jtp.Vector:
+        """"""
+
         R = self.transform()[0:3, 0:3]
 
         to_wxyz = np.array([3, 0, 1, 2])
         return R if dcm else sixd.so3.SO3.from_matrix(R).as_quaternion_xyzw()[to_wxyz]
 
+    @functools.partial(oop.jax_tf.method_ro)
     def transform(self) -> jtp.Matrix:
+        """"""
+
         return self.parent_model.forward_kinematics()[self.index()]
 
+    @functools.partial(oop.jax_tf.method_ro, static_argnames=["vel_repr"])
     def velocity(self, vel_repr: VelRepr = None) -> jtp.Vector:
+        """"""
+
         v_WL = (
             self.jacobian(output_vel_repr=vel_repr)
             @ self.parent_model.generalized_velocity()
         )
+
         return v_WL
 
+    @functools.partial(oop.jax_tf.method_ro, static_argnames=["vel_repr"])
     def linear_velocity(self, vel_repr: VelRepr = None) -> jtp.Vector:
+        """"""
+
         return self.velocity(vel_repr=vel_repr)[0:3]
 
+    @functools.partial(oop.jax_tf.method_ro, static_argnames=["vel_repr"])
     def angular_velocity(self, vel_repr: VelRepr = None) -> jtp.Vector:
+        """"""
+
         return self.velocity(vel_repr=vel_repr)[3:6]
 
+    @functools.partial(oop.jax_tf.method_ro, static_argnames=["output_vel_repr"])
     def jacobian(self, output_vel_repr: VelRepr = None) -> jtp.Matrix:
+        """"""
+
         if output_vel_repr is None:
             output_vel_repr = self.parent_model.velocity_representation
 
-        # Return the doubly left-trivialized free-floating jacobian
+        # Compute the doubly left-trivialized free-floating jacobian
         L_J_WL_B = jacobian(
             model=self.parent_model.physics_model,
             body_index=self.index(),
@@ -148,6 +199,7 @@ class Link(JaxsimDataclass):
         else:
             raise ValueError(output_vel_repr)
 
+    @functools.partial(oop.jax_tf.method_ro)
     def external_force(self) -> jtp.Vector:
         """
         Return the active external force acting on the link.
@@ -169,6 +221,8 @@ class Link(JaxsimDataclass):
     def add_external_force(
         self, force: jtp.Array = None, torque: jtp.Array = None
     ) -> None:
+        """"""
+
         force = force if force is not None else jnp.zeros(3)
         torque = torque if torque is not None else jnp.zeros(3)
 
@@ -207,6 +261,8 @@ class Link(JaxsimDataclass):
     def add_com_external_force(
         self, force: jtp.Array = None, torque: jtp.Array = None
     ) -> None:
+        """"""
+
         force = force if force is not None else jnp.zeros(3)
         torque = torque if torque is not None else jnp.zeros(3)
 
@@ -246,5 +302,8 @@ class Link(JaxsimDataclass):
             )
         )
 
+    @functools.partial(oop.jax_tf.method_ro)
     def in_contact(self) -> jtp.Bool:
+        """"""
+
         return self.parent_model.in_contact()[self.index()]

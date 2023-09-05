d20cae8a6ebe02718dd59e19e33adbb34101346d
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2023-09-05 17:57:56 +0200

    Disable `jax.jit` during motors test

--- tests/test_motor_dynamics.py
+++ tests/test_motor_dynamics.py
@@ -2,7 +2,7 @@ import pathlib
 
 import jax.numpy as jnp
 import numpy as np
-
+import jax
 from jaxsim import high_level
 from jaxsim.high_level.model import Model, ModelData
 from jaxsim.physics.algos.aba import aba
@@ -32,45 +32,96 @@ simulator = JaxSim.build(
 model = simulator.insert_model_from_description(model_description=sdf_path).mutable(
     validate=True
 )
+model.reduce(
+    considered_joints=[
+        "r_shoulder_pitch",
+        "r_shoulder_roll",
+        "r_shoulder_yaw",
+        "r_elbow",
+        "l_shoulder_pitch",
+        "l_shoulder_roll",
+        "l_shoulder_yaw",
+        "l_elbow",
+        "r_hip_pitch",
+        "r_hip_roll",
+        "r_hip_yaw",
+        "r_knee",
+        "r_ankle_pitch",
+        "r_ankle_roll",
+        "l_hip_pitch",
+        "l_hip_roll",
+        "l_hip_yaw",
+        "l_knee",
+        "l_ankle_pitch",
+        "l_ankle_roll",
+        "torso_roll",
+        "torso_yaw",
+    ]
+)
 
+print("Model links: ", model.link_names())
 tau = jnp.array([0.1] * model.dofs())
 
-# Perform FD with CRB without motor
-C_v̇_WB, crb = model.forward_dynamics_crb(tau=tau)
+with jax.disable_jit():
+    print("=====================================================")
+    print("============ Test without motor dynamics ============")
+    print("=====================================================")
 
-print("C_v̇_WB nomotor: ", C_v̇_WB)
-print("a_WB nomotor: ", crb)
+    C_v̇_WB, crb = model.forward_dynamics_crb(tau=tau)
 
-# Set motor parameters
-model.set_motor_gear_ratios(jnp.array([1 / 100.0] * model.dofs()))
-model.set_motor_inertias(jnp.array([0.000_05] * model.dofs()))
-model.set_motor_viscous_frictions(jnp.array([0.0003] * model.dofs()))
+    print("C_v̇_WB: ", C_v̇_WB)
+    print("CRB: ", crb)
 
-# Perform FD with CRB with motor
-C_v̇_WB, crb = model.forward_dynamics_crb(tau=tau)
+    a_WB, aba = model.forward_dynamics_aba(tau=tau)
+    print("a_WB: ", a_WB)
+    print("ABA: ", aba)
 
-print("C_v̇_WB: ", C_v̇_WB)
-print("CRB: ", crb)
+    try:
+        np.testing.assert_allclose(crb, aba, rtol=0.5)
+        np.testing.assert_allclose(C_v̇_WB, a_WB, rtol=0.5)
+    except Exception as e:
+        print(e)
 
-# Perform FD with ABA with motor
-a_WB, aba = model.forward_dynamics_aba(tau=tau)
-print("a_WB: ", a_WB)
-print("ABA: ", aba)
+    print("=====================================================")
+    print("============ Test with motor dynamics ===============")
+    print("=====================================================")
 
-# Set motor inertia to zero and perform FD with CRB and ABA
-model.set_motor_inertias(jnp.array([0.0] * model.dofs()))
-print("Motor inertia set to zero:")
-C_v̇_WB, crb = model.forward_dynamics_crb(tau=tau)
+    # Set motor parameters
+    model.set_motor_gear_ratios(jnp.array([1 / 100.0] * model.dofs()))
+    model.set_motor_inertias(jnp.array([5e-4] * model.dofs()))
+    model.set_motor_viscous_frictions(jnp.array([0.0003] * model.dofs()))
 
-print("C_v̇_WB: ", C_v̇_WB)
-print("CRB: ", crb)
+    C_v̇_WB, crb = model.forward_dynamics_crb(tau=tau)
+    print("C_v̇_WB: ", C_v̇_WB)
+    print("CRB: ", crb)
 
-a_WB, aba = model.forward_dynamics_aba(tau=tau)
-print("a_WB: ", a_WB)
-print("ABA: ", aba)
+    a_WB, aba = model.forward_dynamics_aba(tau=tau)
+    print("a_WB: ", a_WB)
+    print("ABA: ", aba)
 
-try:
-    np.testing.assert_allclose(crb, aba)
-    np.testing.assert_allclose(C_v̇_WB, a_WB)
-except Exception as e:
-    print(e)
+    try:
+        np.testing.assert_allclose(crb, aba, rtol=0.5)
+        np.testing.assert_allclose(C_v̇_WB, a_WB, rtol=0.5)
+    except Exception as e:
+        print(e)
+
+    print("=====================================================")
+    print("================ Inertia set to zero ================")
+    print("=====================================================")
+
+    # Set motor inertia to zero and perform FD with CRB and ABA
+    model.set_motor_inertias(jnp.array([0.0] * model.dofs()))
+
+    C_v̇_WB, crb = model.forward_dynamics_crb(tau=tau)
+    print("C_v̇_WB: ", C_v̇_WB)
+    print("CRB: ", crb)
+
+    a_WB, aba = model.forward_dynamics_aba(tau=tau)
+    print("a_WB: ", a_WB)
+    print("ABA: ", aba)
+
+    try:
+        np.testing.assert_allclose(crb, aba, rtol=0.5)
+        np.testing.assert_allclose(C_v̇_WB, a_WB, rtol=0.5)
+    except Exception as e:
+        print(e)

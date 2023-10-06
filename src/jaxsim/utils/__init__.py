b005b8a2387415b0e8ab5668a483cd9166850d84
diegoferigo <diego.ferigo@iit.it>
2023-10-06 10:31:18 +0200

    Add decorators to apply jit and vmap on jax dataclasses methods

--- src/jaxsim/utils/__init__.py
+++ src/jaxsim/utils/__init__.py
@@ -3,3 +3,6 @@ from jax_dataclasses._copy_and_mutate import _Mutability as Mutability
 from .jaxsim_dataclass import JaxsimDataclass
 from .tracing import not_tracing, tracing
 from .vmappable import Vmappable
+
+# Leave this below the others to prevent circular imports
+from .oop import jax_tf  # isort: skip

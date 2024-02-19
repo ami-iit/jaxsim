d9996af8562bd3e77032595cc8ccc7e3180341a0
diegoferigo <diego.ferigo@iit.it>
2024-02-19 11:36:35 +0100

    Update SoftContactsState class with builder methods

--- src/jaxsim/physics/algos/soft_contacts.py
+++ src/jaxsim/physics/algos/soft_contacts.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import dataclasses
 from typing import Tuple
 
@@ -13,38 +15,67 @@ from jaxsim.math.adjoint import Adjoint
 from jaxsim.math.skew import Skew
 from jaxsim.physics.algos.terrain import FlatTerrain, Terrain
 from jaxsim.physics.model.physics_model import PhysicsModel
+from jaxsim.utils.jaxsim_dataclass import JaxsimDataclass
 
 from . import utils
 
 
 @jax_dataclasses.pytree_dataclass
-class SoftContactsState:
+class SoftContactsState(JaxsimDataclass):
     """
     State of the soft contacts model.
 
     Attributes:
-        tangential_deformation (jtp.Matrix): The tangential deformation of the material at each collidable point.
+        tangential_deformation:
+            The tangential deformation of the material at each collidable point.
     """
 
     tangential_deformation: jtp.Matrix
 
     @staticmethod
+    def build(
+        tangential_deformation: jtp.Matrix | None = None,
+        number_of_collidable_points: int | None = None,
+    ) -> SoftContactsState:
+        """"""
+
+        tangential_deformation = (
+            tangential_deformation
+            if tangential_deformation is not None
+            else jnp.zeros(shape=(3, number_of_collidable_points))
+        )
+
+        return SoftContactsState(
+            tangential_deformation=jnp.array(tangential_deformation, dtype=float)
+        )
+
+    @staticmethod
+    def build_from_physics_model(
+        tangential_deformation: jtp.Matrix | None = None,
+        physics_model: jaxsim.physics.model.physics_model.PhysicsModel | None = None,
+    ) -> SoftContactsState:
+        """"""
+
+        return SoftContactsState.build(
+            tangential_deformation=tangential_deformation,
+            number_of_collidable_points=physics_model.gc.body.size,
+        )
+
+    @staticmethod
     def zero(
         physics_model: jaxsim.physics.model.physics_model.PhysicsModel,
-    ) -> "SoftContactsState":
+    ) -> SoftContactsState:
         """
         Modify the SoftContactsState instance imposing zero tangential deformation.
 
         Args:
-            physics_model (jaxsim.physics.model.physics_model.PhysicsModel): The physics model.
+            physics_model: The physics model.
 
         Returns:
-            SoftContactsState: A SoftContactsState instance with zero tangential deformation.
+            A SoftContactsState instance with zero tangential deformation.
         """
 
-        return SoftContactsState(
-            tangential_deformation=jnp.zeros(shape=(3, physics_model.gc.body.size))
-        )
+        return SoftContactsState.build_from_physics_model(physics_model=physics_model)
 
     def valid(
         self, physics_model: jaxsim.physics.model.physics_model.PhysicsModel
@@ -53,10 +84,10 @@ class SoftContactsState:
         Check if the soft contacts state has valid shape.
 
         Args:
-            physics_model (jaxsim.physics.model.physics_model.PhysicsModel): The physics model.
+            physics_model: The physics model.
 
         Returns:
-            bool: True if the state has a valid shape, otherwise False.
+            True if the state has a valid shape, otherwise False.
         """
 
         from jaxsim.simulation.utils import check_valid_shape
@@ -67,22 +98,6 @@ class SoftContactsState:
             expected_shape=(3, physics_model.gc.body.size),
             valid=True,
         )
-
-    def replace(self, validate: bool = True, **kwargs) -> "SoftContactsState":
-        """
-        Replace attributes of the soft contacts state.
-
-        Args:
-            validate (bool, optional): Whether to validate the state after replacement. Defaults to True.
-
-        Returns:
-            SoftContactsState: A new SoftContactsState instance with replaced attributes.
-        """
-
-        with jax_dataclasses.copy_and_mutate(self, validate=validate) as updated_state:
-            _ = [updated_state.__setattr__(k, v) for k, v in kwargs.items()]
-
-        return updated_state
 
 
 def collidable_points_pos_vel(

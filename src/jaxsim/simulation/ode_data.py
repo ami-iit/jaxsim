340794f4c34ab3de10bf4876e98512d4447f0b8c
diegoferigo <diego.ferigo@iit.it>
2024-02-23 13:45:43 +0100

    Update ODE{State|Input} classes with builder methods

--- src/jaxsim/simulation/ode_data.py
+++ src/jaxsim/simulation/ode_data.py
@@ -13,7 +13,24 @@ from jaxsim.utils import JaxsimDataclass
 
 @jax_dataclasses.pytree_dataclass
 class ODEInput(JaxsimDataclass):
+    """"""
+
     physics_model: PhysicsModelInput
+
+    @staticmethod
+    def build(
+        physics_model_input: PhysicsModelInput | None = None,
+        physics_model: PhysicsModel | None = None,
+    ) -> "ODEInput":
+        """"""
+
+        physics_model_input = (
+            physics_model_input
+            if physics_model_input is not None
+            else PhysicsModelInput.zero(physics_model=physics_model)
+        )
+
+        return ODEInput(physics_model=physics_model_input)
 
     @staticmethod
     def zero(physics_model: PhysicsModel) -> "ODEInput":
@@ -27,8 +44,34 @@ class ODEInput(JaxsimDataclass):
 
 @jax_dataclasses.pytree_dataclass
 class ODEState(JaxsimDataclass):
+    """"""
+
     physics_model: PhysicsModelState
     soft_contacts: SoftContactsState
+
+    @staticmethod
+    def build(
+        physics_model_state: PhysicsModelState | None = None,
+        soft_contacts_state: SoftContactsState | None = None,
+        physics_model: PhysicsModel | None = None,
+    ) -> "ODEState":
+        """"""
+
+        physics_model_state = (
+            physics_model_state
+            if physics_model_state is not None
+            else PhysicsModelState.zero(physics_model=physics_model)
+        )
+
+        soft_contacts_state = (
+            soft_contacts_state
+            if soft_contacts_state is not None
+            else SoftContactsState.zero(physics_model=physics_model)
+        )
+
+        return ODEState(
+            physics_model=physics_model_state, soft_contacts=soft_contacts_state
+        )
 
     @staticmethod
     def deserialize(data: jtp.VectorJax, physics_model: PhysicsModel) -> "ODEState":

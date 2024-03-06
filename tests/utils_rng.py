885de36f93a389cef74b9b25178e3bb1c5668ba6
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-03-06 17:40:10 +0100

    Update RNG utils

--- tests/utils_rng.py
+++ tests/utils_rng.py
@@ -1,12 +1,8 @@
+import jaxlie
 import numpy as np
 
-from jaxsim import logging, sixd
-from jaxsim.physics.model.physics_model import PhysicsModel
-from jaxsim.physics.model.physics_model_state import (
-    PhysicsModelInput,
-    PhysicsModelState,
-)
-from jaxsim.utils import Mutability
+from jaxsim import logging
+from jaxsim.api.model import JaxSimModel
 
 # Initialize a global RNG used by all tests
 test_rng = None
@@ -35,62 +31,58 @@ def get_rng(seed: int = None) -> np.random.Generator:
     return test_rng
 
 
-def random_physics_model_state(physics_model: PhysicsModel) -> PhysicsModelState:
+def random_model_state(
+    model: JaxSimModel,
+) -> dict[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
-    Generate a random `PhysicsModelState` object.
+    Generate a dictionary containing random model state.
 
     Args:
-        physics_model: the physics model to which the random state refers to.
+        model: the model to which the random state refers to.
 
     Returns:
-        The random `PhysicsModelState` object.
+        The dictionary containing the random model state.
     """
 
     rng = get_rng()
 
-    with PhysicsModelState.zero(physics_model=physics_model).mutable_context(
-        mutability=Mutability.MUTABLE
-    ) as state:
-        # Generate random joint quantities
-        state.joint_positions = rng.uniform(size=physics_model.dofs(), low=-1)
-        state.joint_velocities = rng.uniform(size=physics_model.dofs(), low=-1)
-
-        # Generate random base quantities
-        state.base_position = rng.uniform(size=3, low=-1)
-        state.base_quaternion = sixd.so3.SO3.from_rpy_radians(
+    state = {
+        "joint_positions": rng.uniform(size=model.dofs(), low=-1),
+        "joint_velocities": rng.uniform(size=model.dofs(), low=-1),
+        "base_position": rng.uniform(size=3, low=-1),
+        "base_quaternion": jaxlie.SO3.from_rpy_radians(
             *rng.uniform(low=0, high=2 * np.pi, size=3)
-        ).as_quaternion_xyzw()[np.array([3, 0, 1, 2])]
+        ).as_quaternion_xyzw()[np.array([3, 0, 1, 2])],
+    }
 
-        # If floating-base, generate random base velocities
-        if physics_model.is_floating_base:
-            state.base_linear_velocity = rng.uniform(size=3, low=-1)
-            state.base_angular_velocity = rng.uniform(size=3, low=-1)
+    # If floating-base, generate random base velocities
+    if model.floating_base():
+        state | {
+            "base_linear_velocity": rng.uniform(size=3, low=-1),
+            "base_angular_velocity": rng.uniform(size=3, low=-1),
+        }
 
     return state
 
 
-def random_physics_model_input(physics_model: PhysicsModel) -> PhysicsModelInput:
+def random_model_input(model: JaxSimModel) -> dict[np.ndarray, np.ndarray]:
     """
-    Generate a random `PhysicsModelInput` object.
+    Generate a dictionary containing random joint torques and external forces.
 
     Args:
-        physics_model: the physics model to which the random state refers to.
+        model: the model to which the random state refers to.
 
     Returns:
-        The random `PhysicsModelInput` object.
+        A dictionary containing the random joint torques and external forces.
     """
 
     rng = get_rng()
 
-    with PhysicsModelInput.zero(physics_model=physics_model).mutable_context(
-        mutability=Mutability.MUTABLE
-    ) as model_input:
-        # Generate random joint torques and external forces
-        model_input.tau = 10 * rng.uniform(size=physics_model.dofs(), low=-1)
-        model_input.f_ext = 10 * rng.uniform(size=[physics_model.NB, 6], low=-1)
+    tau = 10 * rng.uniform(size=model.dofs(), low=-1)
+    f_ext = 10 * rng.uniform(size=[model.number_of_links(), 6], low=-1)
 
-        # Zero the base force if the robot is fixed base
-        if not physics_model.is_floating_base and physics_model.NB > 0:
-            model_input.f_ext[0] = np.zeros(6)
+    # Zero the base force if the robot is fixed base
+    if not model.floating_base() and model.number_of_links() > 0:
+        f_ext[0] = np.zeros(6)
 
-    return model_input
+    return {"tau": tau, "f_ext": f_ext}

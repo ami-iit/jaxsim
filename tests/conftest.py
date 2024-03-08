d70568ca44abdfb364e0a9827e5fa4cee938b0c5
diegoferigo <diego.ferigo@iit.it>
2024-03-08 16:06:42 +0100

    Add session-wide fixtures to provide tested models

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,13 @@
 import os
+import pathlib
 
 import jax
+import jax.numpy as jnp
 import pytest
+import rod
+
 import jaxsim
+import jaxsim.api as js
 
 
 def pytest_configure() -> None:
@@ -53,3 +58,185 @@ def velocity_representation(request) -> jaxsim.VelRepr:
     """
 
     return request.param
+
+
+# ================================
+# Fixtures providing JaxSim models
+# ================================
+
+# All the fixtures in this section must have "session" scope.
+# In this way, the models are generated only once and shared among all the tests.
+
+
+# This is not a fixture.
+def build_jaxsim_model(
+    model_description: str | pathlib.Path | rod.Model,
+) -> js.model.JaxSimModel:
+    """
+    Helper to build a JaxSim model from a model description.
+
+    Args:
+        model_description: A model description provided by any fixture provider.
+
+    Returns:
+        A JaxSim model built from the provided description.
+    """
+
+    is_urdf = None
+
+    # If the provided description is a string, automatically detect if it
+    # contains the content of a URDF or SDF file.
+    if isinstance(model_description, str):
+        if "<robot" in model_description:
+            is_urdf = True
+
+        elif "<sdf>" in model_description:
+            is_urdf = False
+
+        else:
+            is_urdf = None
+
+    # Build the JaxSim model.
+    model = js.model.JaxSimModel.build_from_model_description(
+        model_description=model_description,
+        gravity=jnp.array([0, 0, -10]),
+        is_urdf=is_urdf,
+    )
+
+    return model
+
+
+@pytest.fixture(scope="session")
+def jaxsim_model_box() -> js.model.JaxSimModel:
+    """
+    Fixture providing the JaxSim model of a box.
+
+    Returns:
+        The JaxSim model of a box.
+    """
+
+    import rod.builder.primitives
+    import rod.urdf.exporter
+
+    # Create on-the-fly a ROD model of a box.
+    rod_model = (
+        rod.builder.primitives.BoxBuilder(x=0.3, y=0.2, z=0.1, mass=1.0, name="box")
+        .build_model()
+        .add_link()
+        .add_inertial()
+        .add_visual()
+        .add_collision()
+        .build()
+    )
+
+    # Export the URDF string.
+    urdf_string = rod.urdf.exporter.UrdfExporter.sdf_to_urdf_string(
+        sdf=rod_model, pretty=True
+    )
+
+    return build_jaxsim_model(model_description=urdf_string)
+
+
+@pytest.fixture(scope="session")
+def jaxsim_model_sphere() -> js.model.JaxSimModel:
+    """
+    Fixture providing the JaxSim model of a sphere.
+
+    Returns:
+        The JaxSim model of a sphere.
+    """
+
+    import rod.builder.primitives
+    import rod.urdf.exporter
+
+    # Create on-the-fly a ROD model of a sphere.
+    rod_model = (
+        rod.builder.primitives.SphereBuilder(radius=0.1, mass=1.0, name="sphere")
+        .build_model()
+        .add_link()
+        .add_inertial()
+        .add_visual()
+        .add_collision()
+        .build()
+    )
+
+    # Export the URDF string.
+    urdf_string = rod.urdf.exporter.UrdfExporter.sdf_to_urdf_string(
+        sdf=rod_model, pretty=True
+    )
+
+    return build_jaxsim_model(model_description=urdf_string)
+
+
+@pytest.fixture(scope="session")
+def jaxsim_model_ergocub() -> js.model.JaxSimModel:
+    """
+    Fixture providing the JaxSim model of the ErgoCub robot.
+
+    Returns:
+        The JaxSim model of the ErgoCub robot.
+    """
+
+    os_environ_original = os.environ.copy()
+
+    try:
+        os.environ["ROBOT_DESCRIPTION_COMMIT"] = "v0.7.1"
+
+        import robot_descriptions.ergocub_description
+    finally:
+        os.environ = os_environ_original
+
+    model_urdf_path = pathlib.Path(
+        robot_descriptions.ergocub_description.URDF_PATH.replace(
+            "ergoCubSN000", "ergoCubSN001"
+        )
+    )
+
+    return build_jaxsim_model(model_description=model_urdf_path)
+
+
+@pytest.fixture(scope="session")
+def jaxsim_model_ergocub_reduced(jaxsim_model_ergocub) -> js.model.JaxSimModel:
+    """
+    Fixture providing the JaxSim model of the ErgoCub robot with only locomotion joints.
+
+    Returns:
+        The JaxSim model of the ErgoCub robot with only locomotion joints.
+    """
+
+    model_full = jaxsim_model_ergocub
+
+    # Get the names of the joints to keep
+    reduced_joints = tuple(
+        j
+        for j in model_full.joint_names()
+        if "camera" not in j
+        # Remove head and hands
+        and "neck" not in j
+        and "wrist" not in j
+        and "thumb" not in j
+        and "index" not in j
+        and "middle" not in j
+        and "ring" not in j
+        and "pinkie" not in j
+        # Remove upper body
+        and "torso" not in j and "elbow" not in j and "shoulder" not in j
+    )
+
+    return js.model.reduce(model=model_full, considered_joints=reduced_joints)
+
+
+@pytest.fixture(scope="session")
+def jaxsim_model_ur10() -> js.model.JaxSimModel:
+    """
+    Fixture providing the JaxSim model of the UR10 robot.
+
+    Returns:
+        The JaxSim model of the UR10 robot.
+    """
+
+    import robot_descriptions.ur10_description
+
+    model_urdf_path = pathlib.Path(robot_descriptions.ur10_description.URDF_PATH)
+
+    return build_jaxsim_model(model_description=model_urdf_path)

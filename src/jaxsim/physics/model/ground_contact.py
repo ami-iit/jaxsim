20a77c266300299d52a243f341d0212a42afca0b
diegoferigo <diego.ferigo@iit.it>
2024-03-12 17:33:59 +0100

    Update GroundContact class

--- src/jaxsim/physics/model/ground_contact.py
+++ src/jaxsim/physics/model/ground_contact.py
@@ -1,53 +1,72 @@
+from __future__ import annotations
+
 import dataclasses
 
 import jax.numpy as jnp
 import jax_dataclasses
-import numpy as np
-import numpy.typing as npt
 from jax_dataclasses import Static
 
+import jaxsim.typing as jtp
 from jaxsim.parsers.descriptions import ModelDescription
 
 
 @jax_dataclasses.pytree_dataclass
 class GroundContact:
     """
-    A class for managing collidable points in a robot model.
+    Class storing the collidable points of a robot model.
 
-    This class is used to store and manage information about collidable points on a robot model,
-    such as their positions and the corresponding bodies (links) they are associated with.
+    This class is used to store and manage information about collidable points
+    of a robot model, such as their positions and the corresponding bodies (links)
+    they are rigidly attached to.
 
     Attributes:
-        point (npt.NDArray): An array of shape (3, N) representing the 3D positions of collidable points.
-        body (Static[npt.NDArray]): An array of integers representing the indices of the bodies (links) associated with each collidable point.
+        point:
+            An array of shape (N, 3) representing the displacement of collidable points
+            w.r.t the origin of their parent body.
+        body:
+            An array of integers representing the indices of the bodies (links)
+            associated to each collidable point.
     """
 
-    point: npt.NDArray = dataclasses.field(default_factory=lambda: jnp.array([]))
-    body: Static[list] = dataclasses.field(default_factory=lambda: [])
+    body: Static[tuple[int, ...]] = dataclasses.field(default_factory=lambda: [])
+
+    point: jtp.Matrix = dataclasses.field(default_factory=lambda: jnp.array([]))
 
     @staticmethod
-    def build_from(
-        model_description: ModelDescription,
-    ) -> "GroundContact":
+    def build_from(model_description: ModelDescription) -> GroundContact:
+        """
+        Build a GroundContact object from a model description.
+
+        Args:
+            model_description: The model description to consider.
+
+        Returns:
+            The GroundContact object.
+        """
+
         if len(model_description.collision_shapes) == 0:
             return GroundContact()
 
-        # Get all the links so that we can take their updated index
+        # Get all the links so that we can take their updated index.
         links_dict = {link.name: link for link in model_description}
 
-        # Get all the enabled collidable points of the model
+        # Get all the enabled collidable points of the model.
         collidable_points = model_description.all_enabled_collidable_points()
 
-        # Build the GroundContact attributes
-        points = jnp.vstack([cp.position for cp in collidable_points]).T
+        # Extract the positions L_p_C of the collidable points w.r.t. the link frames
+        # they are rigidly attached to.
+        points = jnp.vstack([cp.position for cp in collidable_points])
+
+        # Extract the indices of the links to which the collidable points are rigidly
+        # attached to.
         link_index_of_points = [
             links_dict[cp.parent_link.name].index for cp in collidable_points
         ]
 
-        # Build the object
-        gc = GroundContact(point=points, body=link_index_of_points)
+        # Build the GroundContact object.
+        gc = GroundContact(point=points, body=tuple(link_index_of_points))  # noqa
 
-        assert gc.point.shape[0] == 3
-        assert gc.point.shape[1] == len(gc.body)
+        assert gc.point.shape[1] == 3
+        assert gc.point.shape[0] == len(gc.body)
 
         return gc

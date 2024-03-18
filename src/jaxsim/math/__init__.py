d0e7298efb26e842a053f531b579e10ff081fede
diegoferigo <diego.ferigo@iit.it>
2024-03-18 18:08:08 +0100

    Import math resources in parent package

---
+++ src/jaxsim/math/__init__.py
@@ -0,0 +1,7 @@
+from .adjoint import Adjoint
+from .cross import Cross
+from .inertia import Inertia
+from .joint_model import JointModel, supported_joint_motion
+from .quaternion import Quaternion
+from .rotation import Rotation
+from .skew import Skew

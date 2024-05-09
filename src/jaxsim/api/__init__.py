eb5255ffff32698753887171149c5063b70c0539
Alessandro Croci <alessandro.croci@iit.it>
2024-05-09 17:31:47 +0200

    Add `frame` module to `jaxsim.api` package

--- src/jaxsim/api/__init__.py
+++ src/jaxsim/api/__init__.py
@@ -1,3 +1,13 @@
 from . import common  # isort:skip
 from . import model, data  # isort:skip
-from . import com, contact, joint, kin_dyn_parameters, link, ode, ode_data, references
+from . import (
+    com,
+    contact,
+    frame,
+    joint,
+    kin_dyn_parameters,
+    link,
+    ode,
+    ode_data,
+    references,
+)

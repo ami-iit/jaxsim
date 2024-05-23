06564b6ba29f86c7f3188a55508ae34058600584
Alessandro Croci <57228872+xela-95@users.noreply.github.com>
2024-05-23 10:04:59 +0200

    Expose quantities related to generic frames (#148)
    * Create `jaxsim.api.frame` module with `transform` function
    
    * Add `frame` module to `jaxsim.api` package
    
    * Add unit test for `jaxsim.api.frame` module
    
    * Add index-related functions to `frame` module
    
    * Add `test_frame_index` to `frame` unit tests
    
    * Add `jacobian` method to `frame` module
    
    * Add `test_frame_jacobians` to `frame` unit tests
    
    * Add `.vscode` to gitignore
    
    * Add `frame` module to sphynx documentation
    
    * Apply suggestions from code review
    
    Co-authored-by: Filippo Luca Ferretti <102977828+flferretti@users.noreply.github.com>
    Co-authored-by: Diego Ferigo <dgferigo@gmail.com>
    
    * Update frame.py
    
    * Add additional frame attached to link in box model
    
    * Refactor `test_frame_jacobians` to better debug jacobians not matching with iDynTree
    
    * Exclude from `test_frame_jacobians` the frames that are not loaded in KynDynComputations
    
    * Add `frame_parent_link_name` method to `KinDynComputations class`
    
    * Update code style of `frame.transform` function
    
    * WIP Update `test_frame_transforms` to print parent link frames and not fail at the first failed assertion
    
    * Update `test_frame_transforms`
    
    * Add single pendulum fixture in `conftest.py`
    
    * Clean `test_api_frames`
    
    * Fix retrieval of the frame's parent link index
    
    * Add function to get the frame's parent link index
    
    * Update frames test
    
    * Align link and joint tests
    
    * Removed unused tested model
    
    * Update tests to use new ROD URDF exporter function
    
    * Use plain integers for frame indices
    
    * Add JaxSimModel.frame_names
    
    * Fix regression raising TracerBoolConversionError when comparing pytrees
    
    * Temporarily disable test_pytree
    
    ---------
    
    Co-authored-by: Filippo Luca Ferretti <102977828+flferretti@users.noreply.github.com>
    Co-authored-by: Diego Ferigo <dgferigo@gmail.com>
    Co-authored-by: diegoferigo <diego.ferigo@iit.it>

--- src/jaxsim/api/__init__.py
+++ src/jaxsim/api/__init__.py
@@ -3,12 +3,19 @@ from . import model, data  # isort:skip
 from . import (
     com,
     contact,
+<<<<<<< HEAD
+=======
+    frame,
+>>>>>>> e341015 (Expose quantities related to generic frames (#148))
     joint,
     kin_dyn_parameters,
     link,
     ode,
     ode_data,
     references,
+<<<<<<< HEAD
     rigid_contacts,
     soft_contacts,
+=======
+>>>>>>> e341015 (Expose quantities related to generic frames (#148))
 )

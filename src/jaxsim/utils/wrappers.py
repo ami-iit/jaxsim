5f05cf150bc5cf9853cb9b117171bdbdc25b1b63
diegoferigo <diego.ferigo@iit.it>
2024-06-03 17:40:50 +0200

    Update HashlessObject wrapper class

--- src/jaxsim/utils/wrappers.py
+++ src/jaxsim/utils/wrappers.py
@@ -8,6 +8,13 @@ T = TypeVar("T")
 
 @dataclasses.dataclass
 class HashlessObject(Generic[T]):
+    """
+    A class that wraps an object and makes it hashless.
+
+    This is useful for creating particular JAX pytrees.
+    For example, to create a pytree with a static leaf that is ignored
+    by JAX when it compares two instances to trigger a JIT recompilation.
+    """
 
     obj: T
 
@@ -15,4 +22,14 @@ class HashlessObject(Generic[T]):
         return self.obj
 
     def __hash__(self) -> int:
+
         return 0
+
+    def __eq__(self, other: HashlessObject[T]) -> bool:
+
+        if not isinstance(other, HashlessObject) and isinstance(
+            other.get(), type(self.get())
+        ):
+            return False
+
+        return hash(self) == hash(other)

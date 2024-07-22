Fix usage of `Rotation` helper module
Co-authored-by: Diego Ferigo <diego.ferigo@iit.it>

src/jaxsim/math/joint_model.py
@@ -254,8 +254,8 @@ def supported_joint_motion(
         # This is a metadata required by only some joint types.
         axis = jnp.array(joint_axis).astype(float).squeeze()
 
-        pre_H_suc = jaxlie.SE3.from_rotation(
-            rotation=Rotation.from_axis_angle(vector=s * axis)
+        pre_H_suc = jaxlie.SE3.from_matrix(
+            matrix=jnp.eye(4).at[:3, :3].set(Rotation.from_axis_angle(vector=s * axis))
         )
 
         S = jnp.vstack(jnp.hstack([jnp.zeros(3), axis]))

src/jaxsim/math/rotation.py
@@ -79,11 +79,9 @@ class Rotation:
 
             return R.transpose()
 
-        return jaxlie.SO3.from_matrix(
-            jax.lax.cond(
-                pred=(theta == 0.0),
-                true_fun=lambda operand: jnp.eye(3),
-                false_fun=theta_is_not_zero,
-                operand=(theta, vector),
-            )
+        return jax.lax.cond(
+            pred=(theta == 0.0),
+            true_fun=lambda operand: jnp.eye(3),
+            false_fun=theta_is_not_zero,
+            operand=(theta, vector),
         )
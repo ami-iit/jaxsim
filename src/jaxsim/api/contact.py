b04051ae84732a9e62c4810e688fa47985684a06
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-08-14 10:10:33 +0200

    Add the new soft contacts to `api.contact`

--- src/jaxsim/api/contact.py
+++ src/jaxsim/api/contact.py
@@ -131,7 +131,8 @@ def collidable_point_dynamics(
     Returns:
         The 6D force applied to each collidable point and additional data based on the contact model configured:
         - Soft: the material deformation rate.
-        - Rigid: nothing.
+        - Rigid: no additional data.
+        - QuasiRigid: no additional data.
 
     Note:
         The material deformation rate is always returned in the mixed frame
@@ -144,6 +145,10 @@ def collidable_point_dynamics(
     W_p_Ci, W_ṗ_Ci = js.contact.collidable_point_kinematics(model=model, data=data)
 
     # Import privately the contacts classes.
+    from jaxsim.rbda.contacts.quasi_rigid import (
+        QuasiRigidContacts,
+        QuasiRigidContactsState,
+    )
     from jaxsim.rbda.contacts.rigid import RigidContacts, RigidContactsState
     from jaxsim.rbda.contacts.soft import SoftContacts, SoftContactsState
 
@@ -186,6 +191,26 @@ def collidable_point_dynamics(
                 model=model,
                 data=data,
                 link_forces=link_forces,
+            )
+
+            aux_data = dict()
+
+        case QuasiRigidContacts():
+            assert isinstance(model.contact_model, QuasiRigidContacts)
+            assert isinstance(data.state.contact, QuasiRigidContactsState)
+
+            # Build the contact model.
+            quasi_rigid_contacts = QuasiRigidContacts(
+                parameters=data.contacts_params, terrain=model.terrain
+            )
+
+            # Compute the 6D force expressed in the inertial frame and applied to each
+            # collidable point.
+            W_f_Ci, _ = quasi_rigid_contacts.compute_contact_forces(
+                position=W_p_Ci,
+                velocity=W_ṗ_Ci,
+                model=model,
+                data=data,
             )
 
             aux_data = dict()

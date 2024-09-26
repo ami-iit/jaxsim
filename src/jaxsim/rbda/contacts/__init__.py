529028c31a5b480ba9c27a1431034950b85fb349
diegoferigo <diego.ferigo@iit.it>
2024-09-26 14:10:07 +0200

    Import contact-related classes to jaxsim.rbda.contacts package

---
+++ src/jaxsim/rbda/contacts/__init__.py
@@ -0,0 +1,8 @@
+from .common import ContactModel, ContactsParams, ContactsState
+from .relaxed_rigid import (
+    RelaxedRigidContacts,
+    RelaxedRigidContactsParams,
+    RelaxedRigidContactsState,
+)
+from .rigid import RigidContacts, RigidContactsParams, RigidContactsState
+from .soft import SoftContacts, SoftContactsParams, SoftContactsState

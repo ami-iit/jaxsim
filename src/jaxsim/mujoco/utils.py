9ea5c866d17098adc4f2fdb1c666a039c52540d7
diegoferigo <diego.ferigo@iit.it>
2024-10-14 12:53:30 +0200

    Apply suggestions from code review
    Co-authored-by: Filippo Luca Ferretti <102977828+flferretti@users.noreply.github.com>

--- src/jaxsim/mujoco/utils.py
+++ src/jaxsim/mujoco/utils.py
@@ -47,14 +47,14 @@ def mujoco_data_from_jaxsim(
     # Create the helper to operate on the Mujoco model and data.
     model_helper = MujocoModelHelper(model=mujoco_model, data=mujoco_data)
 
-    # Set the model position.
+    # If the model is fixed-base, the Mujoco model won't have the joint corresponding
+    # to the floating base, and the helper would raise an exception.
     if jaxsim_model.floating_base():
 
+        # Set the model position.
         model_helper.set_base_position(position=np.array(jaxsim_data.base_position()))
 
-    # Set the model orientation.
-    if jaxsim_model.floating_base():
-
+        # Set the model orientation.
         model_helper.set_base_orientation(
             orientation=np.array(jaxsim_data.base_orientation())
         )
@@ -83,21 +83,17 @@ def mujoco_data_from_jaxsim(
             if j.name not in set(jaxsim_model.joint_names())
         }
 
-        # Get all the joints in the Mujoco and JaxSim models.
-        joints_mujoco = set(model_helper.joint_names())
-
         # Set the positions of the removed joints.
-        for joint_name in set(joints_removed_dict.keys()):
-
-            if joint_name not in joints_mujoco:
-                continue
-
-            _ = [
-                model_helper.set_joint_position(
-                    position=joints_removed_dict[joint_name].initial_position,
-                    joint_name=joint_name,
-                )
-            ]
+        _ = [
+            model_helper.set_joint_position(
+                position=joints_removed_dict[joint_name].initial_position,
+                joint_name=joint_name,
+            )
+            # Select all original joint that have been removed from the JaxSim model
+            # that are still present in the Mujoco model.
+            for joint_name in joints_removed_dict
+            if joint_name in model_helper.joint_names()
+        ]
 
     # Return the mujoco data with updated kinematics.
     mj.mj_forward(mujoco_model, model_helper.data)

Fix transform computation in visco-elastic contacts

src/jaxsim/rbda/contacts/visco_elastic.py
@@ -1023,7 +1023,7 @@ def step(
     # Compute the link transforms.
     W_H_L = (
         js.model.forward_kinematics(model=model, data=data)
-        if data.velocity_representation is not jaxsim.VelRepr.Mixed
+        if data.velocity_representation is not jaxsim.VelRepr.Inertial
         else jnp.zeros(shape=(model.number_of_links(), 4, 4))
     )
 
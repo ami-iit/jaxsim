aca780764439094856f1e0337bf5009fc8e436f2
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-10-30 17:21:16 +0100

    Fix `ZeroDivision` error in `PlaneTerrain.height`

--- src/jaxsim/terrain/terrain.py
+++ src/jaxsim/terrain/terrain.py
@@ -8,6 +8,7 @@ import jax_dataclasses
 import numpy as np
 
 import jaxsim.typing as jtp
+from jaxsim import exceptions
 
 
 class Terrain(abc.ABC):
@@ -142,6 +143,11 @@ class PlaneTerrain(FlatTerrain):
 
         # Get the plane equation coefficients from the terrain normal.
         A, B, C = self._normal
+
+        exceptions.raise_value_error_if(
+            condition=jnp.allclose(C, 0.0),
+            msg="The z component of the normal cannot be zero.",
+        )
 
         # Compute the final coefficient D considering the terrain height.
         D = -C * self._height

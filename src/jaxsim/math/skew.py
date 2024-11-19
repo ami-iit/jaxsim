6805419b9e70b4a1c34ba326a6935c0515c3c2bd
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-11-19 18:02:39 +0100

    Make `Skew.vee` compatible with batches

--- src/jaxsim/math/skew.py
+++ src/jaxsim/math/skew.py
@@ -37,11 +37,14 @@ class Skew:
             jtp.Vector: The 3D vector extracted from the input matrix.
 
         """
-        vector = 0.5 * jnp.vstack(
+
+        vector = 0.5 * jnp.stack(
             [
-                matrix[2, 1] - matrix[1, 2],
-                matrix[0, 2] - matrix[2, 0],
-                matrix[1, 0] - matrix[0, 1],
-            ]
+                matrix[..., 2, 1] - matrix[..., 1, 2],
+                matrix[..., 0, 2] - matrix[..., 2, 0],
+                matrix[..., 1, 0] - matrix[..., 0, 1],
+            ],
+            axis=-1,
         )
+
         return vector

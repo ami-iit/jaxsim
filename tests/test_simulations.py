f1367b018c4f587985fc355f20503ce6ca7ada12
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-12-09 15:15:45 +0100

    Parametrize simulation test on `integrators` module

--- tests/test_simulations.py
+++ tests/test_simulations.py
@@ -201,8 +201,19 @@ def run_simulation(
     return data
 
 
+@pytest.mark.parametrize(
+    "integrator",
+    [
+        jaxsim.integrators.fixed_step.ForwardEuler,
+        jaxsim.integrators.fixed_step.ForwardEulerSO3,
+        jaxsim.integrators.fixed_step.RungeKutta4,
+        jaxsim.integrators.fixed_step.RungeKutta4SO3,
+        jaxsim.integrators.variable_step.BogackiShampineSO3,
+    ],
+)
 def test_simulation_with_soft_contacts(
     jaxsim_model_box: js.model.JaxSimModel,
+    integrator: jaxsim.integrators.Integrator,
 ):
 
     model = jaxsim_model_box
@@ -217,6 +228,11 @@ def test_simulation_with_soft_contacts(
         enabled_collidable_points_mask[[0, 1, 2, 3]] = True
         model.kin_dyn_parameters.contact_parameters.enabled = tuple(
             enabled_collidable_points_mask.tolist()
+        )
+        model.integrator = integrator.build(
+            dynamics=js.ode.wrap_system_dynamics_for_integration(
+                system_dynamics=js.ode.system_dynamics
+            )
         )
 
     assert np.sum(model.kin_dyn_parameters.contact_parameters.enabled) == 4

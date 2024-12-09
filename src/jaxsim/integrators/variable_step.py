0ba4c7b1b6948b7e3f6c56d086cb9b5e18032adc
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-12-09 15:53:54 +0100

    Fix boolean conversion in variable step integrators

--- src/jaxsim/integrators/variable_step.py
+++ src/jaxsim/integrators/variable_step.py
@@ -481,10 +481,10 @@ class EmbeddedRungeKutta(ExplicitRungeKutta[PyTreeType], Generic[PyTreeType]):
                 metadata_next,
                 discarded_steps,
             ) = jax.lax.cond(
-                pred=discarded_steps
-                >= self.max_step_rejections | local_error
-                <= 1.0 | Δt_next
-                < self.dt_min | integrator_init,
+                pred=(discarded_steps >= self.max_step_rejections)
+                | (local_error <= 1.0)
+                | (Δt_next < self.dt_min)
+                | integrator_init,
                 true_fun=accept_step,
                 false_fun=reject_step,
             )

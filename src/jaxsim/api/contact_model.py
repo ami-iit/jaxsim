78b9d9acb6684a230939b8f3ff582d67699c5604
Alessandro Croci <alessandro.croci@iit.it>
2025-01-21 15:28:52 +0100

    Refactor `link_contact_forces` to compute contact forces in inertial representation and remove unused methods

--- src/jaxsim/api/contact_model.py
+++ src/jaxsim/api/contact_model.py
@@ -6,7 +6,6 @@ import jax.numpy as jnp
 import jaxsim.api as js
 import jaxsim.typing as jtp
 from jaxsim.api.common import ModelDataWithVelocityRepresentation, VelRepr
-from jaxsim.rbda import contacts
 
 
 @jax.jit
@@ -20,111 +19,37 @@ def link_contact_forces(
     **kwargs,
 ) -> jtp.Matrix:
     """
-    Compute the 6D contact forces of all links of the model.
+    Compute the 6D contact forces of all links of the model in inertial representation.
 
     Args:
         model: The model to consider.
         data: The data of the considered model.
         link_forces:
-            The 6D external forces to apply to the links expressed in the same
-            representation of data.
+            The 6D external forces to apply to the links expressed in inertial representation
         joint_force_references:
             The joint force references to apply to the joints.
         kwargs: Additional keyword arguments to pass to the active contact model..
 
     Returns:
         A `(nL, 6)` array containing the stacked 6D contact forces of the links,
-        expressed in the frame corresponding to the active representation.
+        expressed in inertial representation.
     """
 
-    # Build link forces if not provided.
-    # These forces are expressed in the frame corresponding to the velocity
-    # representation of data.
-    O_f_L = (
-        jnp.atleast_2d(link_forces.squeeze())
-        if link_forces is not None
-        else jnp.zeros((model.number_of_links(), 6))
-    ).astype(float)
-
-    # Build joint force references if not provided.
-    joint_force_references = (
-        jnp.atleast_1d(joint_force_references)
-        if joint_force_references is not None
-        else jnp.zeros(model.dofs())
-    )
-
-    # We expect that the 6D forces included in the `link_forces` argument are expressed
-    # in the frame corresponding to the velocity representation of `data`.
-    input_references = js.references.JaxSimModelReferences.build(
+    # Compute the contact forces for each collidable point with the active contact model.
+    W_f_C, _ = model.contact_model.compute_contact_forces(
         model=model,
         data=data,
-        velocity_representation=data.velocity_representation,
-        link_forces=O_f_L,
+        link_forces=link_forces,
         joint_force_references=joint_force_references,
     )
 
     # Compute the 6D forces applied to the links equivalent to the forces applied
     # to the frames associated to the collidable points.
-    f_L, _ = compute_link_contact_forces(
-        model=model,
-        data=data,
-        link_forces=input_references.link_forces(model=model, data=data),
-        joint_force_references=input_references.joint_force_references(),
-        **kwargs,
+    W_f_L = link_forces_from_contact_forces(
+        model=model, data=data, contact_forces=W_f_C
     )
 
-    return f_L
-
-
-@staticmethod
-def compute_link_contact_forces(
-    model: js.model.JaxSimModel,
-    data: js.data.JaxSimModelData,
-    **kwargs,
-) -> tuple[jtp.Matrix, dict[str, jtp.PyTree]]:
-    """
-    Compute the link contact forces.
-
-    Args:
-        model: The robot model considered by the contact model.
-        data: The data of the considered model.
-        **kwargs: Optional additional arguments, specific to the contact model.
-
-    Returns:
-        A tuple containing as first element the 6D contact force applied to the
-        links and expressed in the frame of the velocity representation of data,
-        and as second element a dictionary of optional additional information.
-    """
-
-    # Compute the contact forces expressed in the inertial frame.
-    # This function, contrarily to `compute_contact_forces`, already handles how
-    # the optional kwargs should be passed to the specific contact models.
-    W_f_C, aux_dict = js.contact.collidable_point_dynamics(
-        model=model, data=data, **kwargs
-    )
-
-    # Compute the 6D forces applied to the links equivalent to the forces applied
-    # to the frames associated to the collidable points.
-    with data.switch_velocity_representation(VelRepr.Inertial):
-
-        W_f_L = link_forces_from_contact_forces(
-            model=model, data=data, contact_forces=W_f_C
-        )
-
-    # Store the link forces in the references object for easy conversion.
-    references = js.references.JaxSimModelReferences.build(
-        model=model,
-        data=data,
-        link_forces=W_f_L,
-        velocity_representation=VelRepr.Inertial,
-    )
-
-    # Convert the link forces to the frame corresponding to the velocity
-    # representation of data.
-    with references.switch_velocity_representation(data.velocity_representation):
-        f_L = references.link_forces(model=model, data=data)
-
-    return f_L, aux_dict
+    return W_f_L
 
 
 @staticmethod
@@ -213,90 +138,3 @@ def link_forces_from_contact_forces(
     )(W_f_L, W_H_L)
 
     return f_L
-
-
-@jax.jit
-@js.common.named_scope
-def collidable_point_dynamics(
-    model: js.model.JaxSimModel,
-    data: js.data.JaxSimModelData,
-    link_forces: jtp.MatrixLike | None = None,
-    joint_force_references: jtp.VectorLike | None = None,
-    **kwargs,
-) -> tuple[jtp.Matrix, dict[str, jtp.PyTree]]:
-    r"""
-    Compute the 6D force applied to each enabled collidable point.
-
-    Args:
-        model: The model to consider.
-        data: The data of the considered model.
-        link_forces:
-            The 6D external forces to apply to the links expressed in the same
-            representation of data.
-        joint_force_references:
-            The joint force references to apply to the joints.
-        kwargs: Additional keyword arguments to pass to the active contact model.
-
-    Returns:
-        The 6D force applied to each enabled collidable point and additional data based
-        on the contact model configured:
-        - Soft: the material deformation rate.
-        - Rigid: no additional data.
-        - QuasiRigid: no additional data.
-
-    Note:
-        The material deformation rate is always returned in the mixed frame`
-        `C[W] = ({}^W \mathbf{p}_C, [W])`. This is convenient for integration purpose.
-        Instead, the 6D forces are returned in the active representation.
-    """
-
-    # Build the common kw arguments to pass to the computation of the contact forces.
-    common_kwargs = dict(
-        link_forces=link_forces,
-        joint_force_references=joint_force_references,
-    )
-
-    # Build the additional kwargs to pass to the computation of the contact forces.
-    match model.contact_model:
-
-        case contacts.RelaxedRigidContacts():
-
-            kwargs_contact_model = common_kwargs | kwargs
-
-        case _:
-            raise ValueError(f"Invalid contact model: {model.contact_model}")
-
-    # Compute the contact forces with the active contact model.
-    W_f_C, aux_data = model.contact_model.compute_contact_forces(
-        model=model,
-        data=data,
-        **kwargs_contact_model,
-    )
-
-    # Compute the transforms of the implicit frames `C[L] = (W_p_C, [L])`
-    # associated to the enabled collidable point.
-    # In inertial-fixed representation, the computation of these transforms
-    # is not necessary and the conversion below becomes a no-op.
-
-    # Get the indices of the enabled collidable points.
-    indices_of_enabled_collidable_points = (
-        model.kin_dyn_parameters.contact_parameters.indices_of_enabled_collidable_points
-    )
-
-    W_H_C = (
-        js.contact.transforms(model=model, data=data)
-        if data.velocity_representation is not VelRepr.Inertial
-        else jnp.stack([jnp.eye(4)] * len(indices_of_enabled_collidable_points))
-    )
-
-    # Convert the 6D forces to the active representation.
-    f_Ci = jax.vmap(
-        lambda W_f_C, W_H_C: data.inertial_to_other_representation(
-            array=W_f_C,
-            other_representation=data.velocity_representation,
-            transform=W_H_C,
-            is_force=True,
-        )
-    )(W_f_C, W_H_C)
-
-    return f_Ci, aux_data

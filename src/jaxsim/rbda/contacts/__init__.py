a637eb71009ed6ca2e81072ccb0b603642b84142
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2025-01-30 21:56:24 +0100

    Remove viscoelastic contact model

--- src/jaxsim/rbda/contacts/__init__.py
+++ src/jaxsim/rbda/contacts/__init__.py
@@ -1,13 +1,9 @@
-from . import relaxed_rigid, rigid, soft, visco_elastic
+from . import relaxed_rigid, rigid, soft
 from .common import ContactModel, ContactsParams
 from .relaxed_rigid import RelaxedRigidContacts, RelaxedRigidContactsParams
 from .rigid import RigidContacts, RigidContactsParams
 from .soft import SoftContacts, SoftContactsParams
-from .visco_elastic import ViscoElasticContacts, ViscoElasticContactsParams
 
 ContactParamsTypes = (
-    SoftContactsParams
-    | RigidContactsParams
-    | RelaxedRigidContactsParams
-    | ViscoElasticContactsParams
+    SoftContactsParams | RigidContactsParams | RelaxedRigidContactsParams
 )

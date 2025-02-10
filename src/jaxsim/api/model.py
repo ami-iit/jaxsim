02e51eb3e36da66936f79c1542299106718d908e
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2025-02-10 16:12:16 +0100

    Unify `api.contact_model` in `api.contact`

--- src/jaxsim/api/model.py
+++ src/jaxsim/api/model.py
@@ -2102,7 +2102,7 @@ def step(
 
         # Compute the 6D forces W_f ∈ ℝ^{n_L × 6} applied to links due to contact
         # with the terrain.
-        W_f_L_terrain, aux_dict = js.contact_model.link_contact_forces(
+        W_f_L_terrain, aux_dict = js.contact.link_contact_forces(
             model=model,
             data=data,
             link_forces=W_f_L_external,

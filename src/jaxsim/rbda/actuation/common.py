e0e3286fdf937800e42037d5f18804ab9454ef6a
CarlottaSartore <carlotta.sartore@iit.it>
2025-04-11 16:22:43 +0200

    fix dataclasses usage

--- src/jaxsim/rbda/actuation/common.py
+++ src/jaxsim/rbda/actuation/common.py
@@ -1,9 +1,12 @@
 import dataclasses
 
+import jax_dataclasses
+
 import jaxsim.typing as jtp
 from jaxsim.utils import JaxsimDataclass
 
 
+@jax_dataclasses.pytree_dataclass
 class ActuationParams(JaxsimDataclass):
     """
     Parameters class for the actuation model.

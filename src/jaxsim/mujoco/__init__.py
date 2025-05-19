3a0ebaa20892c57006730d11be35b29323e9ffb6
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2025-05-19 14:14:26 +0100

    Import `MujocoCamera` in `mujoco` module

--- src/jaxsim/mujoco/__init__.py
+++ src/jaxsim/mujoco/__init__.py
@@ -1,4 +1,4 @@
 from .loaders import ModelToMjcf, RodModelToMjcf, SdfToMjcf, UrdfToMjcf
 from .model import MujocoModelHelper
-from .utils import mujoco_data_from_jaxsim
+from .utils import MujocoCamera, mujoco_data_from_jaxsim
 from .visualizer import MujocoVideoRecorder, MujocoVisualizer

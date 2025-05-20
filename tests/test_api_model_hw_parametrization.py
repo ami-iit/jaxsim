597c759a6d56167b683941e38bdf486534ec7a58
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2025-05-20 15:37:20 +0100

    Update hardware parametrization tests

--- tests/test_api_model_hw_parametrization.py
+++ tests/test_api_model_hw_parametrization.py
@@ -95,38 +95,34 @@ def test_model_scaling_against_rod(
     )
 
     # Compare hardware parameters of the scaled JaxSim model with the pre-scaled JaxSim model
-    for link_idx, link_name in enumerate(jaxsim_model_garpez.link_names()):
-        scaled_metadata = jax.tree_util.tree_map(
-            lambda x, link_idx=link_idx: x[link_idx],
-            updated_model.kin_dyn_parameters.hw_link_metadata,
-        )
-        pre_scaled_metadata = jax.tree_util.tree_map(
-            lambda x, link_idx=link_idx: x[link_idx],
-            jaxsim_model_garpez_scaled.kin_dyn_parameters.hw_link_metadata,
-        )
+    scaled_metadata = updated_model.kin_dyn_parameters.hw_link_metadata
 
-        # Compare shape dimensions
-        assert jnp.allclose(scaled_metadata.dims, pre_scaled_metadata.dims, atol=1e-6)
+    pre_scaled_metadata = jaxsim_model_garpez_scaled.kin_dyn_parameters.hw_link_metadata
 
-        # Compare mass
-        scaled_mass, _ = HwLinkMetadata.compute_mass_and_inertia(scaled_metadata)
-        pre_scaled_mass, _ = HwLinkMetadata.compute_mass_and_inertia(
-            pre_scaled_metadata
-        )
-        assert scaled_mass == pytest.approx(pre_scaled_mass, abs=1e-6)
+    # Compare shape dimensions
+    assert jnp.allclose(scaled_metadata.dims, pre_scaled_metadata.dims, atol=1e-6)
 
-        # Compare inertia tensors
-        _, scaled_inertia = HwLinkMetadata.compute_mass_and_inertia(scaled_metadata)
-        _, pre_scaled_inertia = HwLinkMetadata.compute_mass_and_inertia(
-            pre_scaled_metadata
-        )
-        assert jnp.allclose(scaled_inertia, pre_scaled_inertia, atol=1e-6)
+    # Compare mass
+    scaled_mass, _ = HwLinkMetadata.compute_mass_and_inertia(
+        scaled_metadata.shape, scaled_metadata
+    )
+    pre_scaled_mass, _ = HwLinkMetadata.compute_mass_and_inertia(
+        pre_scaled_metadata.shape, pre_scaled_metadata
+    )
+    assert scaled_mass == pytest.approx(pre_scaled_mass, abs=1e-6)
 
-        # Compare transformations
-        assert jnp.allclose(scaled_metadata.L_H_G, pre_scaled_metadata.L_H_G, atol=1e-6)
-        assert jnp.allclose(
-            scaled_metadata.L_H_vis, pre_scaled_metadata.L_H_vis, atol=1e-6
-        )
+    # Compare inertia tensors
+    _, scaled_inertia = HwLinkMetadata.compute_mass_and_inertia(
+        scaled_metadata.shape, scaled_metadata
+    )
+    _, pre_scaled_inertia = HwLinkMetadata.compute_mass_and_inertia(
+        pre_scaled_metadata.shape, pre_scaled_metadata
+    )
+    assert jnp.allclose(scaled_inertia, pre_scaled_inertia, atol=1e-6)
+
+    # Compare transformations
+    assert jnp.allclose(scaled_metadata.L_H_G, pre_scaled_metadata.L_H_G, atol=1e-6)
+    assert jnp.allclose(scaled_metadata.L_H_vis, pre_scaled_metadata.L_H_vis, atol=1e-6)
 
 
 def test_update_hw_parameters_vmap(

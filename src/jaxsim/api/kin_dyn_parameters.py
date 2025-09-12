93c16cbbff9da81d7ad45098e680f6dc659bfe4d
Filippo Luca Ferretti <filippo.ferretti@iit.it>
2025-09-12 16:51:00 +0200

    Update `ContactParameters` class and add collision shape map

--- src/jaxsim/api/kin_dyn_parameters.py
+++ src/jaxsim/api/kin_dyn_parameters.py
@@ -14,8 +14,15 @@ import jaxsim
 import jaxsim.typing as jtp
 from jaxsim.math import Inertia, JointModel, supported_joint_motion
 from jaxsim.math.adjoint import Adjoint
-from jaxsim.parsers.descriptions import JointDescription, JointType, ModelDescription
-from jaxsim.utils import HashedNumpyArray, JaxsimDataclass
+from jaxsim.parsers.descriptions import (
+    JointDescription,
+    JointType,
+    ModelDescription,
+    SphereCollision,
+    BoxCollision,
+    CylinderCollision,
+)
+from jaxsim.utils import HashedNumpyArray, JaxsimDataclass, CollidableShapeType
 
 
 @jax_dataclasses.pytree_dataclass(eq=False, unsafe_hash=False)
@@ -762,6 +769,13 @@ class LinkParameters(JaxsimDataclass):
         return jnp.atleast_2d(jnp.where(I, I, I.T)).astype(float)
 
 
+_COLLISION_SHAPE_MAP = {
+    SphereCollision: CollidableShapeType.Sphere,
+    BoxCollision: CollidableShapeType.Box,
+    CylinderCollision: CollidableShapeType.Cylinder,
+}
+
+
 @jax_dataclasses.pytree_dataclass
 class ContactParameters(JaxsimDataclass):
     """
@@ -783,18 +797,9 @@ class ContactParameters(JaxsimDataclass):
         to be created with vmap. This is because the `body` attribute must be `Static`.
     """
 
-    body: Static[tuple[int, ...]] = dataclasses.field(default_factory=tuple)
-
-    point: jtp.Matrix = dataclasses.field(default_factory=lambda: jnp.array([]))
-
-    enabled: Static[tuple[bool, ...]] = dataclasses.field(default_factory=tuple)
-
-    @property
-    def indices_of_enabled_collidable_points(self) -> npt.NDArray:
-        """
-        Return the indices of the enabled collidable points.
-        """
-        return np.where(np.array(self.enabled))[0]
+    center: jtp.Vector = dataclasses.field(default_factory=lambda: jnp.array([]))
+    shape_size: jtp.Vector = dataclasses.field(default_factory=lambda: jnp.array([]))
+    shape_type: jtp.Vector = dataclasses.field(default_factory=lambda: jnp.array([]))
 
     @staticmethod
     def build_from(model_description: ModelDescription) -> ContactParameters:
@@ -811,33 +816,27 @@ class ContactParameters(JaxsimDataclass):
         if len(model_description.collision_shapes) == 0:
             return ContactParameters()
 
-        # Get all the links so that we can take their updated index.
-        links_dict = {link.name: link for link in model_description}
-
-        # Get all the enabled collidable points of the model.
-        collidable_points = model_description.all_enabled_collidable_points()
-
-        # Extract the positions L_p_C of the collidable points w.r.t. the link frames
-        # they are rigidly attached to.
-        points = jnp.vstack([cp.position for cp in collidable_points])
-
-        # Extract the indices of the links to which the collidable points are rigidly
-        # attached to.
-        link_index_of_points = tuple(
-            links_dict[cp.parent_link.name].index for cp in collidable_points
+        # Assume the link_parameters and the collision_shapes are in the same order.
+        centers = jnp.array(
+            [shape.center for shape in model_description.collision_shapes]
         )
+
+        shape_size = jnp.array(
+            [shape.size.squeeze() for shape in model_description.collision_shapes]
+        )
+
+        shape_type = [
+            _COLLISION_SHAPE_MAP[type(shape)]
+            for shape in model_description.collision_shapes
+        ]
+        shape_type = jnp.array(shape_type, dtype=int)
 
         # Build the ContactParameters object.
-        cp = ContactParameters(
-            point=points,
-            body=link_index_of_points,
-            enabled=tuple(True for _ in link_index_of_points),
+        return ContactParameters(
+            center=centers,
+            shape_type=shape_type,
+            shape_size=shape_size,
         )
-
-        assert cp.point.shape[1] == 3, cp.point.shape[1]
-        assert cp.point.shape[0] == len(cp.body), cp.point.shape[0]
-
-        return cp
 
 
 @jax_dataclasses.pytree_dataclass

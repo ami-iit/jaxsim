cd6ed53c94ddf614a1b24ab4d8064c3a2d90121a
Filippo Luca Ferretti <filippo.ferretti@iit.it>
2025-09-12 16:39:40 +0200

    Add `CollidableShapeType` enum

--- src/jaxsim/utils/__init__.py
+++ src/jaxsim/utils/__init__.py
@@ -3,3 +3,17 @@ from jax_dataclasses._copy_and_mutate import _Mutability as Mutability
 from .jaxsim_dataclass import JaxsimDataclass
 from .tracing import not_tracing, tracing
 from .wrappers import HashedNumpyArray, HashlessObject
+
+from typing import ClassVar
+
+
+# TODO (flferretti): Definetely not the best place for this
+class CollidableShapeType:
+    """
+    Enum representing the types of collidable shapes.
+    """
+
+    Sphere: ClassVar[int] = 0
+    Box: ClassVar[int] = 1
+    Cylinder: ClassVar[int] = 2
+    Unsupported: ClassVar[int] = -1

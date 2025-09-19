e1f001ad49e2649bbcfb4117e8dd76e569b93521
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2025-09-19 19:55:27 +0200

    Consider collision shape offset wrt link center

--- src/jaxsim/rbda/contacts/relaxed_rigid.py
+++ src/jaxsim/rbda/contacts/relaxed_rigid.py
@@ -332,6 +332,7 @@ class RelaxedRigidContacts(common.ContactModel):
             common.compute_penetration_data, in_axes=(None,)
         )(
             model,
+            shape_offset=model.kin_dyn_parameters.contact_parameters.center,
             shape_type=model.kin_dyn_parameters.contact_parameters.shape_type,
             shape_size=model.kin_dyn_parameters.contact_parameters.shape_size,
             link_transforms=data._link_transforms,
